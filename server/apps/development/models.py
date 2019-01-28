c6135ee47507ba5dce3303cfab49e76bdfa24b7c
Stanislav Lapshin <staslapshin88@gmail.com>
2019-01-28 16:53:37 +0300

    [#] Flake8 fix

--- server/apps/development/models.py
+++ server/apps/development/models.py
@@ -101,7 +101,7 @@ class Issue(Notable,
                                                 help_text=_('HT__TIME_ESTIMATE'))
 
     total_spent = models.PositiveIntegerField(null=True, verbose_name=_('VN__TOTAL_SPENT'),
-                                                   help_text=_('HT__TOTAL_SPENT'))
+                                              help_text=_('HT__TOTAL_SPENT'))
 
     employee = models.ForeignKey(User, models.SET_NULL, null=True, blank=True, verbose_name=_('VN__EMPLOYEE'),
                                  help_text=_('HT__EMPLOYEE'))
