d1db8c58a928097033e09a20cd8a0c035a405ca1
Stanislav Lapshin <staslapshin88@gmail.com>
2019-02-01 17:50:57 +0300

    [#] Add test for user specific metric

--- server/apps/development/tests/test_api_metrics_days.py
+++ server/apps/development/tests/test_api_metrics_days.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from apps.core.tests.base import BaseAPITest
 from apps.development.models import Note
 from apps.development.tests.factories import IssueFactory, IssueNoteFactory
+from apps.users.tests.factories import UserFactory
 
 
 class ApiMetricsDaysTests(BaseAPITest):
@@ -53,6 +54,32 @@ class ApiMetricsDaysTests(BaseAPITest):
 
         self._check_metric(response.data[0], timezone.now() - timedelta(days=1), timedelta(hours=1))
         self._check_metric(response.data[1], timezone.now() + timedelta(days=1), timedelta(hours=3))
+
+    def test_another_user(self):
+        another_user = UserFactory.create()
+
+        self._create_note(Note.TYPE.time_spend, timezone.now() - timedelta(days=2, hours=5), timedelta(hours=2))
+        self._create_note(Note.TYPE.time_spend, timezone.now() - timedelta(days=1), timedelta(hours=4),
+                          user=another_user)
+        self._create_note(Note.TYPE.time_spend, timezone.now() - timedelta(days=1, hours=5), -timedelta(hours=3))
+        self._create_note(Note.TYPE.time_spend, timezone.now() + timedelta(days=1), timedelta(hours=3),
+                          user=another_user)
+
+        IssueFactory.create()
+
+        self.set_credentials()
+        response = self.client.get('/api/metrics', {
+            'user': self.user.id,
+            'start': self.format_date(timezone.now() - timedelta(days=5)),
+            'end': self.format_date(timezone.now() + timedelta(days=5)),
+            'group': 'day'
+        })
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+        self._check_metric(response.data[0], timezone.now() - timedelta(days=2), timedelta(hours=2))
+        self._check_metric(response.data[1], timezone.now() - timedelta(days=1), -timedelta(hours=3))
 
     def _create_note(self, note_type, created_at, spent: timedelta = None, user=None):
         return IssueNoteFactory.create(type=note_type,
