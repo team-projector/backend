4c7cb7c63b0b82915d87e85090b152fb3899087e
Stanislav Lapshin <staslapshin88@gmail.com>
2019-02-01 17:33:36 +0300

    [#] Spends fix

--- server/apps/development/tests/test_parse_spend.py
+++ server/apps/development/tests/test_parse_spend.py
@@ -2,15 +2,16 @@ from datetime import timedelta
 
 from django.test import TestCase
 
-from apps.development.utils.parsers import parse_spend
+from apps.development.utils.notes import parse_spend
 
 PARSE_MAP = [
-    ('1d 1m', timedelta(days=1, minutes=1)),
-    (' 1d  1m  5s', timedelta(days=1, minutes=1, seconds=5)),
+    ('1d 1m', timedelta(hours=8, minutes=1)),
+    (' 1d  1m  5s', timedelta(hours=8, minutes=1, seconds=5)),
     ('1m', timedelta(minutes=1)),
-    ('1d 30m', timedelta(days=1, minutes=30)),
-    ('1d 30m 15s', timedelta(days=1, minutes=30, seconds=15)),
-    ('2w 2d 4h', timedelta(weeks=2, days=2, hours=4)),
+    ('1d 30m', timedelta(hours=8, minutes=30)),
+    ('1d 30m 15s', timedelta(hours=8, minutes=30, seconds=15)),
+    ('2w 2d 4h', timedelta(hours=100)),  # 2*5d * 8h + 2*8h + 4h
+    ('2mo 2d 4h', timedelta(hours=340)),  # 2*4w*5d*8*h + 2*8h + 4h
     ('0', timedelta(seconds=0)),
     ('', timedelta(seconds=0)),
     (None, timedelta(seconds=0)),
--- server/apps/development/utils/notes.py
+++ server/apps/development/utils/notes.py
@@ -1,13 +1,71 @@
 import re
-from collections import namedtuple
-from typing import Optional, Pattern
-
-from .parsers import parse_spend
+from collections import defaultdict, namedtuple
+from datetime import timedelta
+from typing import DefaultDict, Optional, Pattern
 
 RE_SPEND: Pattern = re.compile(r'^(?P<action>(added|subtracted)) (?P<spent>.+) of time spent at \d{4}-\d{2}-\d{2}$')
+RE_SPEND_PART: Pattern = re.compile(r'(?P<value>\d+)(?P<part>(mo|w|d|h|m|s))')
 SPEND_RESET_MESSAGE = 'removed time spent'
 
+WEEK_PER_MONTH = 4
+DAYS_PER_WEEK = 5
+HOURS_PER_DAY = 8
+
+
+def seconds_handler(bag: DefaultDict[str, int], val: int) -> None:
+    bag['seconds'] += val
+
+
+def minutes_handler(bag: DefaultDict[str, int], val: int) -> None:
+    bag['minutes'] += val
+
+
+def hours_handler(bag: DefaultDict[str, int], val: int) -> None:
+    bag['hours'] += val
+
+
+def days_handler(bag: DefaultDict[str, int], val: int) -> None:
+    bag['hours'] += val * HOURS_PER_DAY
+
+
+def weeks_handler(bag: DefaultDict[str, int], val: int) -> None:
+    bag['hours'] += val * DAYS_PER_WEEK * HOURS_PER_DAY
+
+
+def months_handler(bag: DefaultDict[str, int], val: int) -> None:
+    bag['hours'] += val * WEEK_PER_MONTH * DAYS_PER_WEEK * HOURS_PER_DAY
+
+
+GITLAB_SPEND_HANDLERS = {
+    'mo': months_handler,
+    'w': weeks_handler,
+    'd': days_handler,
+    'h': hours_handler,
+    'm': minutes_handler,
+    's': seconds_handler
+}
+
 NoteReadResult = namedtuple('NoteReadResult', ['type', 'data'])
+
+
+def parse_spend(s: str) -> int:
+    # spec https://docs.gitlab.com/ee/workflow/time_tracking.html
+    s = s or ''
+    s = s.strip()
+
+    if not s:
+        return 0
+
+    bag = defaultdict(int)
+
+    for part in s.split(' '):
+        m = RE_SPEND_PART.match(part)
+        if not m:
+            continue
+
+        GITLAB_SPEND_HANDLERS[m.group('part')](bag, int(m.group('value')))
+
+    return int(timedelta(**bag).total_seconds())
 
 
 class BaseNoteParser:
