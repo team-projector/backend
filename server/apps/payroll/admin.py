9cef2b282f1fac76e971242338b86a9d142e9860
Stanislav Lapshin <staslapshin88@gmail.com>
2019-02-02 22:09:34 +0300

    [#35] Admin improve

--- server/apps/payroll/admin.py
+++ server/apps/payroll/admin.py
@@ -6,6 +6,6 @@ from .models import SpentTime
 
 @admin.register(SpentTime)
 class SpentTimeAdmin(BaseModelAdmin):
-    list_display = ('employee', 'date', 'time_spent')
+    list_display = ('employee', 'date', 'content_type', 'object_id', 'time_spent')
     search_fields = ('employee',)
     autocomplete_fields = ('note', 'employee')
