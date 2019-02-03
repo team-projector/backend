4d8b06040e4318234627cd7ed3c59991886a8c1a
Vitaly Velikodny <vvelikodny@gmail.com>
2019-02-04 02:12:39 +0300

    fix uniq value for factory

--- server/apps/users/tests/factories.py
+++ server/apps/users/tests/factories.py
@@ -4,7 +4,7 @@ from apps.users.models import User
 
 
 class UserFactory(factory.django.DjangoModelFactory):
-    login = factory.Faker('word')
+    login = factory.Sequence(lambda n: 'User {0}'.format(n))
     name = factory.Faker('name')
     hour_rate = factory.Faker('random_int', min=0, max=999)
     is_staff = False
