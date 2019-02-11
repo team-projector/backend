7167e3dbdf118c20cce63325567265292bdeedd5
Stanislav Lapshin <staslapshin88@gmail.com>
2019-02-11 18:13:03 +0300

    [#45] Add efficiency in weeks metrics

--- server/apps/payroll/tests/test_api_metrics_weeks.py
+++ server/apps/payroll/tests/test_api_metrics_weeks.py
@@ -32,7 +32,7 @@ class ApiMetricsWeeksTests(BaseAPITest):
         self.issue.time_estimate = timedelta(hours=15).total_seconds()
         self.issue.total_time_spent = self.issue.time_spents.aggregate(spent=Sum('time_spent'))['spent']
         self.issue.state = 'opened'
-        self.issue.due_date = timezone.now() + timedelta(days=1)
+        self.issue.due_date = monday + timedelta(days=1)
         self.issue.save()
 
         self.set_credentials()
@@ -56,6 +56,42 @@ class ApiMetricsWeeksTests(BaseAPITest):
                                 monday: 1
                             }, {
                                 monday: timedelta(hours=15)
+                            }, {})
+
+    def test_efficiency(self):
+        monday = begin_of_week(timezone.now().date())
+
+        self._create_spent_time(monday + timedelta(days=4), timedelta(hours=3))
+        self._create_spent_time(monday + timedelta(days=2, hours=5), timedelta(hours=2))
+        self._create_spent_time(monday + timedelta(days=1), timedelta(hours=4))
+        self._create_spent_time(monday + timedelta(days=1, hours=5), -timedelta(hours=3))
+
+        self.issue.time_estimate = timedelta(hours=15).total_seconds()
+        self.issue.total_time_spent = self.issue.time_spents.aggregate(spent=Sum('time_spent'))['spent']
+        self.issue.state = 'closed'
+        self.issue.due_date = monday + timedelta(days=1)
+        self.issue.closed_at = monday + timedelta(days=1)
+        self.issue.save()
+
+        self.set_credentials()
+        start = monday - timedelta(days=5)
+        end = monday + timedelta(days=5)
+
+        response = self.client.get('/api/metrics', {
+            'user': self.user.id,
+            'start': self.format_date(start),
+            'end': self.format_date(end),
+            'group': 'week'
+        })
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+        self._check_metrics(response.data,
+                            {
+                                monday: timedelta(hours=6)
+                            }, {}, {}, {
+                                monday: self.issue.total_time_spent / self.issue.time_estimate
                             })
 
     def test_many_weeks(self):
@@ -94,7 +130,7 @@ class ApiMetricsWeeksTests(BaseAPITest):
                                 monday: 1
                             }, {
                                 monday: timedelta(hours=15)
-                            })
+                            }, {})
 
     def test_not_in_range(self):
         monday = begin_of_week(timezone.now().date())
@@ -107,7 +143,7 @@ class ApiMetricsWeeksTests(BaseAPITest):
         self.issue.time_estimate = timedelta(hours=15).total_seconds()
         self.issue.total_time_spent = self.issue.time_spents.aggregate(spent=Sum('time_spent'))['spent']
         self.issue.state = 'opened'
-        self.issue.due_date = timezone.now() + timedelta(days=1)
+        self.issue.due_date = monday + timedelta(days=1)
         self.issue.save()
 
         self.set_credentials()
@@ -131,7 +167,7 @@ class ApiMetricsWeeksTests(BaseAPITest):
                                 monday: 1
                             }, {
                                 monday: timedelta(hours=15)
-                            })
+                            }, {})
 
     def test_another_user(self):
         another_user = UserFactory.create()
@@ -146,7 +182,7 @@ class ApiMetricsWeeksTests(BaseAPITest):
         self.issue.time_estimate = timedelta(hours=15).total_seconds()
         self.issue.total_time_spent = self.issue.time_spents.aggregate(spent=Sum('time_spent'))['spent']
         self.issue.state = 'opened'
-        self.issue.due_date = timezone.now() + timedelta(days=1)
+        self.issue.due_date = monday + timedelta(days=1)
         self.issue.save()
 
         self.set_credentials()
@@ -170,7 +206,7 @@ class ApiMetricsWeeksTests(BaseAPITest):
                                 monday: 1
                             }, {
                                 monday: timedelta(hours=15)
-                            })
+                            }, {})
 
     def test_many_issues(self):
         monday = begin_of_week(timezone.now().date())
@@ -188,7 +224,7 @@ class ApiMetricsWeeksTests(BaseAPITest):
         self.issue.time_estimate = timedelta(hours=15).total_seconds()
         self.issue.total_time_spent = self.issue.time_spents.aggregate(spent=Sum('time_spent'))['spent']
         self.issue.state = 'opened'
-        self.issue.due_date = timezone.now() + timedelta(days=1)
+        self.issue.due_date = monday + timedelta(days=1)
         self.issue.save()
 
         another_issue.total_time_spent = another_issue.time_spents.aggregate(spent=Sum('time_spent'))['spent']
@@ -215,7 +251,7 @@ class ApiMetricsWeeksTests(BaseAPITest):
                                 monday: 2
                             }, {
                                 monday: timedelta(days=1, hours=1)
-                            })
+                            }, {})
 
     def _create_spent_time(self, date, spent: timedelta = None, user=None, issue=None):
         return IssueSpentTimeFactory.create(date=date,
@@ -226,7 +262,8 @@ class ApiMetricsWeeksTests(BaseAPITest):
     def _check_metrics(self, metrics,
                        spents: Dict[date, timedelta],
                        issues_counts: Dict[date, int],
-                       time_estimates: Dict[date, timedelta]):
+                       time_estimates: Dict[date, timedelta],
+                       efficiencies: Dict[date, float]):
 
         spents = {
             self.format_date(d): time
@@ -241,6 +278,11 @@ class ApiMetricsWeeksTests(BaseAPITest):
         issues_counts = {
             self.format_date(d): count
             for d, count in issues_counts.items()
+        }
+
+        efficiencies = {
+            self.format_date(d): ef
+            for d, ef in efficiencies.items()
         }
 
         for metric in metrics:
@@ -269,6 +311,18 @@ class ApiMetricsWeeksTests(BaseAPITest):
                                  f'bad time_estimate for {metric["start"]}: '
                                  f'expected - 0, '
                                  f'actual - {timedelta(seconds=metric["time_estimate"])}')
+
+            if metric['start'] in efficiencies:
+                self.assertEqual(metric['efficiency'],
+                                 efficiencies[metric['start']],
+                                 f'bad efficiency for {metric["start"]}: '
+                                 f'expected - {efficiencies[metric["start"]]}, '
+                                 f'actual - {metric["efficiency"]}')
+            else:
+                self.assertEqual(metric['efficiency'], 0,
+                                 f'bad efficiency for {metric["start"]}: '
+                                 f'expected - 0, '
+                                 f'actual - {metric["efficiency"]}')
 
             if metric['start'] in issues_counts:
                 self.assertEqual(metric['issues'], issues_counts[metric['start']])
