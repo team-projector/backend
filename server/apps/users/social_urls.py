5cbd5046eedcc857f5e4e1a12733477a83f1110c
Stanislav Lapshin <staslapshin88@gmail.com>
2019-03-07 18:05:50 +0300

    [#] improve urls

--- server/apps/users/social_urls.py
+++ server/apps/users/social_urls.py
@@ -1,20 +1,13 @@
-"""URLs module"""
-from django.conf import settings
 from django.urls import path
-from social_core.utils import setting_name
 from social_django import views
 
 from . import social_views
 
-extra = getattr(settings, setting_name('TRAILING_SLASH'), True) and '/' or ''
-
 app_name = 'social'
 
 urlpatterns = [
-    # authentication / association
     path('login/<slug:backend>/', views.auth, name='begin'),
     path('complete/<slug:backend>/', social_views.complete, name='complete'),
-    # disconnection
     path('disconnect/<slug:backend>/', views.disconnect, name='disconnect'),
     path('disconnect/<slug:backend>/<int:association_id>/', views.disconnect, name='disconnect_individual'),
 ]
