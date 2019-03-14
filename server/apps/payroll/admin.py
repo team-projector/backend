fdc03c8f34a5e65423dfacec9bfb94ba4eb41f40
Stanislav Lapshin <staslapshin88@gmail.com>
2019-03-14 18:11:38 +0300

    [#61] Implement salary datamodel

--- server/apps/payroll/admin.py
+++ server/apps/payroll/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 from apps.core.admin.base import BaseModelAdmin
-from .models import SpentTime
+from apps.users.admin import UserFilter
+from .models import SpentTime, Salary, Payroll, Bonus, Penalty, Payment
 
 
 @admin.register(SpentTime)
@@ -9,3 +10,43 @@ class SpentTimeAdmin(BaseModelAdmin):
     list_display = ('user', 'date', 'content_type', 'object_id', 'time_spent')
     search_fields = ('user',)
     autocomplete_fields = ('note', 'user')
+
+
+@admin.register(Salary)
+class SalaryAdmin(BaseModelAdmin):
+    list_display = ('user', 'created_by', 'created_at', 'total', 'payed')
+    list_filter = (UserFilter,)
+    search_fields = ('user',)
+    autocomplete_fields = ('user', 'created_by')
+
+
+@admin.register(Payroll)
+class PayrollAdmin(BaseModelAdmin):
+    list_display = ('user', 'created_by', 'created_at', 'sum')
+    list_filter = (UserFilter,)
+    search_fields = ('user',)
+    autocomplete_fields = ('user', 'created_by', 'salary')
+
+
+@admin.register(Bonus)
+class BonusAdmin(BaseModelAdmin):
+    list_display = ('user', 'created_by', 'created_at', 'sum')
+    list_filter = (UserFilter,)
+    search_fields = ('user',)
+    autocomplete_fields = ('user', 'created_by', 'salary')
+
+
+@admin.register(Penalty)
+class PenaltyAdmin(BaseModelAdmin):
+    list_display = ('user', 'created_by', 'created_at', 'sum')
+    list_filter = (UserFilter,)
+    search_fields = ('user',)
+    autocomplete_fields = ('user', 'created_by', 'salary')
+
+
+@admin.register(Payment)
+class PaymentAdmin(BaseModelAdmin):
+    list_display = ('user', 'created_by', 'created_at', 'sum')
+    list_filter = (UserFilter,)
+    search_fields = ('user',)
+    autocomplete_fields = ('user', 'created_by', 'salary')
--- server/apps/payroll/models.py
+++ server/apps/payroll/models.py
@@ -1,10 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from apps.core.db.fields import MoneyField
+from apps.core.db.mixins import Timestamps
 from apps.development.models import Note
-from apps.users.models import User
+
+User = get_user_model()
 
 
 class SpentTime(models.Model):
@@ -13,6 +17,7 @@ class SpentTime(models.Model):
 
     date = models.DateField(null=True)
 
+    rate = models.FloatField(null=True, verbose_name=_('VN__RATE'), help_text=_('HT__RATE'))
     time_spent = models.IntegerField(verbose_name=_('VN__TIME_SPENT'), help_text=_('HT__TIME_SPENT'))
 
     user = models.ForeignKey(User, models.CASCADE, verbose_name=_('VN__USER'), help_text=_('HT__USER'))
@@ -30,5 +35,78 @@ class SpentTime(models.Model):
 
     class Meta:
         verbose_name = _('VN__SPENT_TIME')
-        verbose_name_plural = _('VN__SPENT_TIME')
+        verbose_name_plural = _('VN__SPENT_TIMES')
         ordering = ('-date',)
+
+
+class Payroll(Timestamps):
+    created_by = models.ForeignKey(User, models.CASCADE, verbose_name=_('VN__CREATED_BY'),
+                                   help_text=_('HT__CREATED_BY'))
+    sum = MoneyField(verbose_name=_('VN__SUM'), help_text=_('HT__SUM'))
+    salary = models.ForeignKey('Salary', models.SET_NULL, null=True, blank=True, related_name='+',
+                               verbose_name=_('VN__SALARY'), help_text=_('HT__SALARY'))
+
+    user = models.ForeignKey(User, models.CASCADE, related_name='+', verbose_name=_('VN__USER'),
+                             help_text=_('HT__USER'))
+
+    def __str__(self):
+        return f'{self.user} [{self.created_at}]: {self.sum}'
+
+    class Meta:
+        verbose_name = _('VN__PAYROLL')
+        verbose_name_plural = _('VN__PAYROLLS')
+        ordering = ('-created_at',)
+
+
+class Bonus(Payroll):
+    description = models.TextField(verbose_name=_('VN__DESCRIPTION'), help_text=_('HT__DESCRIPTION'))
+
+    class Meta:
+        verbose_name = _('VN__BONUS')
+        verbose_name_plural = _('VN__BONUSES')
+        ordering = ('-created_at',)
+
+
+class Penalty(Payroll):
+    description = models.TextField(verbose_name=_('VN__DESCRIPTION'), help_text=_('HT__DESCRIPTION'))
+
+    class Meta:
+        verbose_name = _('VN__PENALTY')
+        verbose_name_plural = _('VN__PENALTIES')
+        ordering = ('-created_at',)
+
+
+class Payment(Payroll):
+    class Meta:
+        verbose_name = _('VN__PAYMENT')
+        verbose_name_plural = _('VN__PAYMENTS')
+        ordering = ('-created_at',)
+
+
+class Salary(Timestamps):
+    created_by = models.ForeignKey(User, models.CASCADE, verbose_name=_('VN__CREATED_BY'),
+                                   help_text=_('HT__CREATED_BY'))
+    user = models.ForeignKey(User, models.CASCADE, related_name='salaries',
+                             verbose_name=_('VN__USER'), help_text=_('HT__USER'))
+
+    period_from = models.DateField(null=True, blank=True, verbose_name=_('VN__PERIOD_FROM'),
+                                   help_text=_('HT__PERIOD_FROM'))
+    period_to = models.DateField(null=True, blank=True, verbose_name=_('VN__PERIOD_TO'),
+                                 help_text=_('HT__PERIOD_TO'))
+
+    charged_time = models.IntegerField(verbose_name=_('VN__CHARGED_TIME'), help_text=_('HT__CHARGED_TIME'))
+
+    taxes = MoneyField(verbose_name=_('VN__TAXES'), help_text=_('HT__TAXES'))
+    bonus = MoneyField(verbose_name=_('VN__BONUS'), help_text=_('HT__BONUS'))
+    sum = MoneyField(verbose_name=_('VN__SUM'), help_text=_('HT__SUM'))
+    total = MoneyField(verbose_name=_('VN__TOTAL'), help_text=_('HT__TOTAL'))
+
+    payed = models.BooleanField(default=False, verbose_name=_('VN__PAYED'), help_text=_('HT__PAYED'))
+
+    def __str__(self):
+        return f'{self.user} [{self.created_at}]: {self.sum}'
+
+    class Meta:
+        verbose_name = _('VN__SALARY')
+        verbose_name_plural = _('VN__SALARIES')
+        ordering = ('-created_at',)
