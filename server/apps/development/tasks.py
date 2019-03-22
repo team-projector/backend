99e97cdc88579acbbe4ed1286afbf4a51d74f900
Stanislav Lapshin <staslapshin88@gmail.com>
2019-03-22 09:38:20 +0300

    [#] sync in `low_priority` worker

--- server/apps/development/tasks.py
+++ server/apps/development/tasks.py
@@ -5,7 +5,7 @@ from apps.development.utils.loaders import load_groups, load_project_issue, \
 from celery_app import app
 
 
-@app.task(exchange='low_priority')
+@app.task(queue='low_priority')
 def sync() -> None:
     load_groups()
     load_projects()
@@ -13,13 +13,13 @@ def sync() -> None:
     sync_issues.delay()
 
 
-@app.task(exchange='low_priority')
+@app.task(queue='low_priority')
 def sync_issues() -> None:
     for project_id in Project.objects.values_list('id', flat=True):
         sync_project_issues.delay(project_id)
 
 
-@app.task(exchange='low_priority')
+@app.task(queue='low_priority')
 def sync_project_issues(project_id: int) -> None:
     project = Project.objects.get(id=project_id)
 
