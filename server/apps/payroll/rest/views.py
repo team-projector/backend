e8f2004642c492bb2d79ed6dc4d30ff7f3d23790
Stanislav Lapshin <staslapshin88@gmail.com>
2019-04-03 21:53:26 +0300

    [#69] Salaries list

--- server/apps/payroll/rest/views.py
+++ server/apps/payroll/rest/views.py
@@ -8,8 +8,9 @@ from rest_framework.response import Response
 from apps.core.rest.views import BaseGenericAPIView
 from apps.core.utils.rest import parse_query_params
 from apps.payroll.rest.permissions import CanViewUserMetrics
-from .serializers import TimeExpenseSerializer, UserProgressMetricsParamsSerializer, UserProgressMetricsSerializer
-from ..models import SpentTime
+from .serializers import SalarySerializer, TimeExpenseSerializer, UserProgressMetricsParamsSerializer, \
+    UserProgressMetricsSerializer
+from ..models import Salary, SpentTime
 from ..utils.metrics.progress import create_progress_calculator
 
 User = get_user_model()
@@ -32,6 +33,26 @@ class UserProgressMetricsView(BaseGenericAPIView):
         metrics = calculator.calculate()
 
         return Response(UserProgressMetricsSerializer(metrics, many=True).data)
+
+
+class UserSalariesView(mixins.ListModelMixin,
+                       BaseGenericAPIView):
+    permission_classes = (permissions.IsAuthenticated, CanViewUserMetrics)
+    queryset = Salary.objects.all()
+    serializer_class = SalarySerializer
+
+    @cached_property
+    def user(self):
+        user = get_object_or_404(User.objects, pk=self.kwargs['user_pk'])
+        self.check_object_permissions(self.request, user)
+
+        return user
+
+    def filter_queryset(self, queryset):
+        return super().filter_queryset(queryset).filter(user=self.user)
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
 
 class TimeExpensesView(mixins.ListModelMixin,
---
+++ server/apps/payroll/tests/test_api_user_salaries.py
@@ -0,0 +1,62 @@
+from rest_framework import status
+
+from apps.core.tests.base import BaseAPITest
+from apps.development.models import TeamMember
+from apps.development.tests.factories import TeamFactory, TeamMemberFactory
+from apps.payroll.tests.factories import SalaryFactory
+from apps.payroll.utils.metrics.user import User
+
+
+class UserSalariesTests(BaseAPITest):
+    def setUp(self):
+        super().setUp()
+
+        self.user = User.objects.create_user(login='user')
+
+    def test_list(self):
+        SalaryFactory.create_batch(10, user=self.user)
+
+        self.set_credentials()
+        response = self.client.get(f'/api/users/{self.user.id}/salaries')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 10)
+
+    def test_another_user(self):
+        SalaryFactory.create_batch(10, user=User.objects.create_user(login='user_2'))
+
+        self.set_credentials()
+        response = self.client.get(f'/api/users/{self.user.id}/salaries')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 0)
+
+    def test_bad_user(self):
+        user_2 = User.objects.create_user(login='user_2')
+        SalaryFactory.create_batch(10, user=user_2)
+
+        self.set_credentials()
+        response = self.client.get(f'/api/users/{user_2.id}/salaries')
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_bad_user_but_teamlead(self):
+        user_2 = self.create_user('user_2@mail.com')
+
+        team = TeamFactory.create()
+
+        TeamMemberFactory.create(team=team,
+                                 user=self.user,
+                                 roles=TeamMember.roles.developer | TeamMember.roles.leader)
+
+        TeamMemberFactory.create(team=team,
+                                 user=user_2,
+                                 roles=TeamMember.roles.developer)
+
+        SalaryFactory.create_batch(10, user=user_2)
+
+        self.set_credentials()
+        response = self.client.get(f'/api/users/{user_2.id}/salaries')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 10)
