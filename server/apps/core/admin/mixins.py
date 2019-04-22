70a8378edebafe5d08f81afdf683a811cb2dd45f
Alexandr Girin <alex.girin.web@gmail.com>
2019-04-22 17:09:56 +0300

    [#62] delete get_verbose_name function

--- server/apps/core/admin/mixins.py
+++ server/apps/core/admin/mixins.py
@@ -19,21 +19,16 @@ class ForceSyncEntityMixin(BaseModelAdmin):
             request, object_id, form_url, extra_context=extra_context,
         )
 
-    def get_verbose_name(self, obj):
-        verbose_name = obj._meta.verbose_name
-        return verbose_name
-
-    def sync_handler(self, obj):
-        raise NotImplementedError
-
-    def sync_obj(self, request, obj):
-        self.sync_handler(obj)
-        verbose_name = self.get_verbose_name(obj)
-        self.message_user(request, f'{verbose_name} "{obj}" is syncing')
-
     def response_change(self, request, obj):
         if '_force_sync' in request.POST:
-            self.sync_obj(request, obj)
+            self._sync_obj(request, obj)
             return HttpResponseRedirect(request.path)
 
         return super().response_change(request, obj)
+
+    def _sync_obj(self, request, obj):
+        self.sync_handler(obj)
+        self.message_user(request, f'{obj._meta.verbose_name} "{obj}" is syncing')
+
+    def sync_handler(self, obj):
+        raise NotImplementedError
