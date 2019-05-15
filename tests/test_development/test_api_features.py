77613f4b69d0290f1d7a18c29daf4c74258cfcab
Vyacheslav Bondarenko <noszlodno@gmail.com>
2019-05-15 16:48:54 +0300

    [#104] added fields to test_api_features.

--- tests/test_development/test_api_features.py
+++ tests/test_development/test_api_features.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from rest_framework import status
 
 from apps.development.models import TeamMember
@@ -17,6 +18,8 @@ class ApiIssuesTests(BaseAPITest):
     def get_data(self):
         return {
             'title': 'Feature 1',
+            'start_date': str(timezone.now().date()),
+            'due_date': str(timezone.now().date() + timezone.timedelta(days=1)),
             'milestone': self.milestone.id,
         }
 
@@ -28,3 +31,6 @@ class ApiIssuesTests(BaseAPITest):
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['title'], data['title'])
+        self.assertEqual(response.data['start_date'], data['start_date'])
+        self.assertEqual(response.data['due_date'], data['due_date'])
+        self.assertEqual(response.data['milestone']['id'], data['milestone'])
