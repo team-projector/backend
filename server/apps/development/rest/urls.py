b694612235aa3fd118394d417789f94f81303652
Vyacheslav Bondarenko <noszlodno@gmail.com>
2019-05-19 17:53:59 +0300

    [#97] after merge

--- server/apps/development/rest/urls.py
+++ server/apps/development/rest/urls.py
@@ -4,7 +4,7 @@ from apps.core.rest.routers import AppRouter
 from .views import (
     IssuesViewset, gl_webhook, TeamsViewset, TeamMembersViewset, ProjectGroupMilestonesViewset,
     ProjectMilestonesViewset, MilestoneIssuesViewset, MilestoneFeaturesViewset, FeaturesViewset, FeatureIssuesViewset,
-    GitlabStatusView, MilestonesViewset, GitlabIssueStatusView
+    GitlabStatusView, MilestonesViewset, GitlabIssueStatusView, TeamIssuesViewset
 )
 
 app_name = 'development'
@@ -23,6 +23,7 @@ router.register(r'^milestones/(?P<milestone_pk>\d+)/issues$', MilestoneIssuesVie
 router.register(r'^milestones/(?P<milestone_pk>\d+)/features', MilestoneFeaturesViewset,
                 basename='features-issues')
 router.register(r'^teams/(?P<team_pk>\d+)/members$', TeamMembersViewset, basename='team-members')
+router.register(r'^teams/(?P<team_pk>\d+)/issues$', TeamIssuesViewset, basename='team-issues')
 router.register('teams', TeamsViewset, basename='teams')
 router.register('milestones', MilestonesViewset, basename='milestones')
 
--- server/apps/development/rest/views.py
+++ server/apps/development/rest/views.py
@@ -234,3 +234,19 @@ class GitlabIssueStatusView(BaseGenericAPIView):
     def get(self, request, format=None):
         queryset = self.filter_queryset(self.get_queryset())
         return Response(self.get_serializer(queryset, many=True).data)
+
+
+class TeamIssuesViewset(mixins.ListModelMixin,
+                        BaseGenericViewSet):
+    serializer_classes = {
+        'list': IssueCardSerializer
+    }
+    queryset = Issue.objects
+    filter_backends = (filters.SearchFilter, DjangoFilterBackend)
+
+    search_fields = ('title',)
+    filter_fields = ('state', 'due_date', 'user')
+    ordering_fields = ('due_date', 'title', 'created_at')
+
+    def filter_queryset(self, queryset):
+        return super().filter_queryset(queryset).filter(user__team_members__team_id=self.kwargs['team_pk'])
