3dadc63a624e1e244ac9773acf715fb9fee39373
Vyacheslav Bondarenko <noszlodno@gmail.com>
2019-05-23 07:03:02 +0300

    [#102] merged

--- server/apps/development/rest/views.py
+++ server/apps/development/rest/views.py
@@ -188,6 +188,24 @@ class MilestonesViewset(mixins.ListModelMixin,
     filter_backends = (MilestoneActiveFiler,)
 
 
+class MilestoneIssuesOrphanViewset(mixins.ListModelMixin,
+                                   BaseGenericViewSet):
+    permission_classes = (permissions.IsProjectManager,)
+
+    serializer_classes = {
+        'list': IssueCardSerializer
+    }
+
+    queryset = Issue.objects.all()
+
+    @cached_property
+    def milestone(self):
+        return get_object_or_404(Milestone.objects, pk=self.kwargs['milestone_pk'])
+
+    def filter_queryset(self, queryset):
+        return super().filter_queryset(queryset.filter(milestone=self.milestone, feature__isnull=True))
+
+
 class GitlabIssueStatusView(BaseGenericAPIView):
     serializer_class = GitlabIssieStatusSerializer
     queryset = Issue.objects.all()
