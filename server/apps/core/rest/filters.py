6a3b02c6e2f4b3a8f18d9e70a15d198edd4546be
Vyacheslav Bondarenko <noszlodno@gmail.com>
2019-05-23 19:22:13 +0300

    [#117] remove validate_url

--- server/apps/core/rest/filters.py
+++ server/apps/core/rest/filters.py
@@ -1,12 +1,5 @@
-from django.conf import settings
 from rest_framework import serializers
 
 
 class FilterParamUrlSerializer(serializers.Serializer):
     url = serializers.URLField()
-
-    def validate_url(self, value):
-        if settings.GITLAB_HOST not in value:
-            raise serializers.ValidationError('invalid url param')
-
-        return value
