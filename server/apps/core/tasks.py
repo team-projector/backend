f747f79364493b00e29844f02943ea7259754adb
Stanislav Lapshin <staslapshin88@gmail.com>
2019-05-29 10:38:25 +0300

    [#] `add_action` task is priority

--- server/apps/core/tasks.py
+++ server/apps/core/tasks.py
@@ -4,7 +4,7 @@ from apps.users.models import User
 from celery_app import app
 
 
-@app.task(queue='low_priority')
+@app.task
 def add_action(**kwargs) -> None:
     # TODO: need default sender
     sender_id = kwargs.pop('sender_id', None)
