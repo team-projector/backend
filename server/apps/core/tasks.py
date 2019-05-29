5ceed7d61b6148039b04e910292c156f405f8f32
Stanislav Lapshin <staslapshin88@gmail.com>
2019-05-29 12:57:06 +0300

    [#] Add system user

--- server/apps/core/tasks.py
+++ server/apps/core/tasks.py
@@ -6,8 +6,4 @@ from celery_app import app
 
 @app.task
 def add_action(**kwargs) -> None:
-    # TODO: need default sender
-    sender_id = kwargs.pop('sender_id', None)
-    sender = User.objects.get(id=sender_id) if sender_id else User.objects.order_by('id').first()
-
-    action.send(sender, **kwargs)
+    action.send(kwargs.pop('sender_id', User.objects.system_user), **kwargs)
