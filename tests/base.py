f9509e4a419359b84e87dc60b5c925268e76b975
Vyacheslav Bondarenko <noszlodno@gmail.com>
2019-06-06 08:14:29 +0300

    [#129] add gitlab mock, tests issues/<pk>/spend

--- tests/base.py
+++ tests/base.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 import sys
+import httpretty
 
 from django.db import transaction
 from rest_framework.test import APITestCase
@@ -75,6 +76,33 @@ class BaseAPITest(BaseTestMixin, APITestCase):
 
     def clear_client_headers(self):
         self.client._credentials = {}
+
+
+class HttpPrettyTests:
+    def registry_get_url(self, url, status, body=None):
+        httpretty.enable()
+        self._register_head_url(url, status=status, body=body)
+        self._register_get_url(url, status=status, body=body)
+
+    def registry_post_url(self, url, status, body=None):
+        httpretty.enable()
+        self._register_post_url(url, status=status, body=body)
+
+    @staticmethod
+    def disable_url():
+        httpretty.disable()
+
+    @staticmethod
+    def _register_head_url(url, **kwargs):
+        httpretty.register_uri(httpretty.HEAD, url, **kwargs)
+
+    @staticmethod
+    def _register_get_url(url, **kwargs):
+        httpretty.register_uri(httpretty.GET, url, **kwargs)
+
+    @staticmethod
+    def _register_post_url(url, **kwargs):
+        httpretty.register_uri(httpretty.POST, url, **kwargs)
 
 
 def format_date(d: datetime) -> str:
