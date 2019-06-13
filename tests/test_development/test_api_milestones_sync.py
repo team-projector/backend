6fc0f6c41aeddd6e79cfe7c83c9381d1ae0637fe
Vyacheslav Bondarenko <noszlodno@gmail.com>
2019-06-13 13:31:14 +0300

    [#124] add tests gitlab milestone sync

--- tests/test_development/test_api_milestones_sync.py
+++ tests/test_development/test_api_milestones_sync.py
@@ -1,11 +1,18 @@
+import httpretty
+
+from django.test import override_settings
 from rest_framework import status
 
 from tests.base import BaseAPITest
-from tests.test_development.factories import ProjectGroupMilestoneFactory
+from tests.test_development.factories import (
+    ProjectGroupFactory, ProjectGroupMilestoneFactory, ProjectFactory, ProjectMilestoneFactory)
+from tests.test_development.mocks import registry_get_gl_url
+from tests.test_development.factories_gitlab import (
+    AttrDict, GlUserFactory, GlGroupFactory, GlGroupMilestoneFactory, GlProjectFactory, GlProjectMilestoneFactory)
 
 
 class ApiMilestoneSyncTests(BaseAPITest):
-    def test_list_not_allowed(self):
+    def test_not_allowed(self):
         milestone = ProjectGroupMilestoneFactory.create()
 
         self.set_credentials()
@@ -13,10 +20,58 @@ class ApiMilestoneSyncTests(BaseAPITest):
 
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-    def test_list_not_found(self):
+    def test_not_found(self):
         milestone = ProjectGroupMilestoneFactory.create()
 
         self.set_credentials()
         response = self.client.post(f'/api/milestones/{milestone.id + 1}/sync')
 
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    @override_settings(GITLAB_TOKEN='GITLAB_TOKEN')
+    @httpretty.activate
+    def test_sync_group(self):
+        gl_group = AttrDict(GlGroupFactory())
+        group = ProjectGroupFactory.create(gl_id=gl_group.id)
+
+        gl_milestone = AttrDict(GlGroupMilestoneFactory(state='closed'))
+        milestone = ProjectGroupMilestoneFactory.create(gl_id=gl_milestone.id, owner=group, state='active')
+
+        registry_get_gl_url('https://gitlab.com/api/v4/user', GlUserFactory())
+        registry_get_gl_url(f'https://gitlab.com/api/v4/groups/{gl_group.id}', gl_group)
+        registry_get_gl_url(f'https://gitlab.com/api/v4/groups/{gl_group.id}/milestones/{gl_milestone.id}',
+                            gl_milestone)
+
+        self.set_credentials()
+        response = self.client.post(f'/api/milestones/{milestone.id}/sync')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], milestone.id)
+        self.assertEqual(response.data['gl_id'], milestone.gl_id)
+
+        milestone.refresh_from_db()
+        self.assertEqual(milestone.state, 'closed')
+
+    @override_settings(GITLAB_TOKEN='GITLAB_TOKEN')
+    @httpretty.activate
+    def test_sync_project(self):
+        gl_project = AttrDict(GlProjectFactory())
+        project = ProjectFactory.create(gl_id=gl_project.id)
+
+        gl_milestone = AttrDict(GlProjectMilestoneFactory(state='closed'))
+        milestone = ProjectMilestoneFactory.create(gl_id=gl_milestone.id, owner=project, state='active')
+
+        registry_get_gl_url('https://gitlab.com/api/v4/user', GlUserFactory())
+        registry_get_gl_url(f'https://gitlab.com/api/v4/projects/{gl_project.id}', gl_project)
+        registry_get_gl_url(f'https://gitlab.com/api/v4/projects/{gl_project.id}/milestones/{gl_milestone.id}',
+                            gl_milestone)
+
+        self.set_credentials()
+        response = self.client.post(f'/api/milestones/{milestone.id}/sync')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], milestone.id)
+        self.assertEqual(response.data['gl_id'], milestone.gl_id)
+
+        milestone.refresh_from_db()
+        self.assertEqual(milestone.state, 'closed')
