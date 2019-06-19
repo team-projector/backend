9bbcca5437acab5739f3a8a9cb5a1c28df87405c
Vyacheslav Bondarenko <noszlodno@gmail.com>
2019-06-19 08:03:20 +0300

    [#133] seperate function for filtering active user admin

--- server/apps/users/admin/main.py
+++ server/apps/users/admin/main.py
@@ -66,13 +66,17 @@ class UserAdmin(AdminFormFieldsOverridesMixin,
         referer = request.META.get('HTTP_REFERER')
         referer_path = urlparse(referer).path
 
+        self._filter_is_active_true_by_default(request, referer, referer_path)
+
+        return super().changelist_view(request, extra_context)
+
+    @staticmethod
+    def _filter_is_active_true_by_default(request, referer, referer_path):
         if not referer or referer_path != reverse('admin:users_user_changelist'):
             q = request.GET.copy()
             q['is_active__exact'] = '1'
             request.GET = q
             request.META['QUERY_STRING'] = request.GET.urlencode()
-
-        return super().changelist_view(request, extra_context)
 
 
 @admin.register(Group)
