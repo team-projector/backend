454012785c83b81ba0555d13334e1cbad619dfb4
Vyacheslav Bondarenko <noszlodno@gmail.com>
2019-06-24 14:39:11 +0300

    [#146] add link to payroll edit from

--- server/apps/payroll/admin/main.py
+++ server/apps/payroll/admin/main.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.admin import helpers
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models.fields.related import OneToOneRel
 from django.shortcuts import redirect, render
+from django.utils.html import mark_safe
 from django.urls import path, reverse
 
 from apps.core.admin.base import BaseModelAdmin
@@ -48,10 +51,35 @@ class SalaryAdmin(BaseModelAdmin):
 
 @admin.register(Payroll)
 class PayrollAdmin(BaseModelAdmin):
-    list_display = ('user', 'created_by', 'created_at', 'sum')
+    list_display = ('user', 'created_at', 'sum',)
     list_filter = (UserFilter,)
     search_fields = ('user',)
-    autocomplete_fields = ('user', 'created_by', 'salary')
+    autocomplete_fields = ('user', 'salary')
+    readonly_fields = ('inheritance',)
+
+    def inheritance(self, payroll):
+        node = None
+        for field in self._get_accessor_name_fields(self.model):
+            try:
+                node = getattr(payroll, field)
+                if node:
+                    break
+            except ObjectDoesNotExist:
+                pass
+
+        if node:
+            url = reverse(f'admin:{node._meta.app_label}_{node._meta.model_name}_change', args=[node.id])
+
+            return mark_safe(f'<a href={url}>{node._meta.model_name.capitalize()}: {str(node)}</a>')
+
+    @staticmethod
+    def _get_accessor_name_fields(model):
+        related_objects = [
+            f for f in model._meta.get_fields()
+            if isinstance(f, OneToOneRel) and issubclass(f.field.model, model)
+        ]
+
+        return [rel.get_accessor_name() for rel in related_objects]
 
 
 @admin.register(SpentTime)
