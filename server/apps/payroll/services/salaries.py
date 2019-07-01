b7c728fd03adcdae01fc67697d33f5ec3bd07ebe
Andrey Surovov <andrey.surovov@gmail.com>
2019-07-01 13:45:00 +0300

    [#156] refactor filter_available_salaries

--- server/apps/payroll/services/salaries.py
+++ server/apps/payroll/services/salaries.py
@@ -1,15 +1,13 @@
-from django.db.models import Q
-
 from apps.development.models import TeamMember
-from apps.development.services.team_members import filter_by_roles
 
 
 def filter_available_salaries(queryset, user):
-    teams = filter_by_roles(
-        TeamMember.objects.filter(user=user),
-        [TeamMember.roles.leader]
-    ).values_list('team_id', flat=True)
+    users = TeamMember.objects.filter(
+        user=user,
+        roles=TeamMember.roles.leader
+    ).values_list(
+        'team__members',
+        flat=True
+    )
 
-    users_query = Q(user__teams__in=teams) | Q(user=user.id)
-
-    return queryset.filter(users_query).distinct()
+    return queryset.filter(user__in=(*users, user.id))
