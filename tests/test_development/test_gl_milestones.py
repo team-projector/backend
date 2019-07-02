a9f09290fb732c9d63e6ac62fadc767ba862a900
Vyacheslav Bondarenko <noszlodno@gmail.com>
2019-07-02 12:38:21 +0300

    [#140] use gitlab mocker in milestones tests

--- tests/test_development/test_gl_milestones.py
+++ tests/test_development/test_gl_milestones.py
@@ -1,28 +1,25 @@
 from django.test import override_settings
 
+from apps.development.models import Milestone
 from apps.development.services.gitlab.milestones import (
     load_group_milestone, load_project_milestone, load_group_milestones, load_gl_project_milestones
 )
-from apps.development.models import Milestone
-
 from tests.test_development.factories import ProjectFactory, ProjectGroupFactory
 from tests.test_development.factories_gitlab import (
     AttrDict, GlUserFactory, GlGroupFactory, GlProjectFactory, GlProjectMilestoneFactory,
 )
-from tests.test_development.mocks import activate_httpretty, registry_get_gl_url
 
 
 @override_settings(GITLAB_TOKEN='GITLAB_TOKEN')
-@activate_httpretty
-def test_load_group_milestone(db):
-    registry_get_gl_url('https://gitlab.com/api/v4/user', GlUserFactory())
+def test_load_group_milestone(db, gl_mocker):
+    gl_mocker.registry_get('/user', GlUserFactory())
 
     gl_group = AttrDict(GlGroupFactory())
     group = ProjectGroupFactory.create(gl_id=gl_group.id)
-    registry_get_gl_url(f'https://gitlab.com/api/v4/groups/{gl_group.id}', gl_group)
+    gl_mocker.registry_get(f'/groups/{gl_group.id}', gl_group)
 
     gl_milestone = AttrDict(GlProjectMilestoneFactory())
-    registry_get_gl_url(f'https://gitlab.com/api/v4/groups/{gl_group.id}/milestones/{gl_milestone.id}', gl_milestone)
+    gl_mocker.registry_get(f'/groups/{gl_group.id}/milestones/{gl_milestone.id}', gl_milestone)
 
     load_group_milestone(group, gl_group.id, gl_milestone.id)
 
@@ -31,17 +28,15 @@ def test_load_group_milestone(db):
 
 
 @override_settings(GITLAB_TOKEN='GITLAB_TOKEN')
-@activate_httpretty
-def test_load_project_milestone(db):
-    registry_get_gl_url('https://gitlab.com/api/v4/user', GlUserFactory())
+def test_load_project_milestone(db, gl_mocker):
+    gl_mocker.registry_get('/user', GlUserFactory())
 
     gl_project = AttrDict(GlProjectFactory())
     project = ProjectFactory.create(gl_id=gl_project.id)
-    registry_get_gl_url(f'https://gitlab.com/api/v4/projects/{gl_project.id}', gl_project)
+    gl_mocker.registry_get(f'/projects/{gl_project.id}', gl_project)
 
     gl_milestone = AttrDict(GlProjectMilestoneFactory())
-    registry_get_gl_url(f'https://gitlab.com/api/v4/projects/{gl_project.id}/milestones/{gl_milestone.id}',
-                        gl_milestone)
+    gl_mocker.registry_get(f'/projects/{gl_project.id}/milestones/{gl_milestone.id}', gl_milestone)
 
     load_project_milestone(project, gl_project.id, gl_milestone.id)
 
@@ -50,16 +45,15 @@ def test_load_project_milestone(db):
 
 
 @override_settings(GITLAB_TOKEN='GITLAB_TOKEN')
-@activate_httpretty
-def test_load_group_milestones(db):
-    registry_get_gl_url('https://gitlab.com/api/v4/user', GlUserFactory())
+def test_load_group_milestones(db, gl_mocker):
+    gl_mocker.registry_get('/user', GlUserFactory())
 
     gl_group = AttrDict(GlGroupFactory())
     group = ProjectGroupFactory.create(gl_id=gl_group.id)
-    registry_get_gl_url(f'https://gitlab.com/api/v4/groups/{gl_group.id}', gl_group)
+    gl_mocker.registry_get(f'/groups/{gl_group.id}', gl_group)
 
     gl_milestone = AttrDict(GlProjectMilestoneFactory())
-    registry_get_gl_url(f'https://gitlab.com/api/v4/groups/{gl_group.id}/milestones', [gl_milestone])
+    gl_mocker.registry_get(f'/groups/{gl_group.id}/milestones', [gl_milestone])
 
     load_group_milestones(group.id, gl_group.id)
 
@@ -68,16 +62,15 @@ def test_load_group_milestones(db):
 
 
 @override_settings(GITLAB_TOKEN='GITLAB_TOKEN')
-@activate_httpretty
-def test_load_gl_project_milestones(db):
-    registry_get_gl_url('https://gitlab.com/api/v4/user', GlUserFactory())
+def test_load_gl_project_milestones(db, gl_mocker):
+    gl_mocker.registry_get('/user', GlUserFactory())
 
     gl_project = AttrDict(GlGroupFactory())
     project = ProjectFactory.create(gl_id=gl_project.id)
-    registry_get_gl_url(f'https://gitlab.com/api/v4/projects/{gl_project.id}', gl_project)
+    gl_mocker.registry_get(f'/projects/{gl_project.id}', gl_project)
 
     gl_milestone = AttrDict(GlProjectMilestoneFactory())
-    registry_get_gl_url(f'https://gitlab.com/api/v4/projects/{gl_project.id}/milestones', [gl_milestone])
+    gl_mocker.registry_get(f'/projects/{gl_project.id}/milestones', [gl_milestone])
 
     load_gl_project_milestones(project.id, gl_project.id)
 
