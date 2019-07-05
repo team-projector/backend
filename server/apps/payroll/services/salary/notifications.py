872453baea77f92de0e59f91148397079da70425
Vyacheslav Bondarenko <noszlodno@gmail.com>
2019-07-05 07:05:23 +0300

    [#148] add Slack client

--- server/apps/payroll/services/salary/notifications.py
+++ server/apps/payroll/services/salary/notifications.py
@@ -1,12 +1,11 @@
-from apps.core.slack.base import (
-    get_users_channel_by_email, send_message_to_channel
-)
+from apps.core.slack.client import Slack
 
 
 def send_message_to_slack(email):
     msg = 'Salary has been paid.'
 
-    channel = get_users_channel_by_email(email)
+    slack = Slack()
+    channel = slack.get_users_channel_by_email(email)
 
     if channel:
-        send_message_to_channel(channel['id'], msg)
+        slack.send_message_to_channel(channel['id'], msg)
