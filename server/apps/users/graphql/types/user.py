42aeb2f925a3828098714c21cf427c6cf15c113c
Stanislav Lapshin <staslapshin88@gmail.com>
2019-07-12 21:00:35 +0300

    [#] gql: Add problemsto user

--- server/apps/users/graphql/types/user.py
+++ server/apps/users/graphql/types/user.py
@@ -5,11 +5,13 @@ from apps.core.graphql.connection import DataSourceConnection
 from apps.core.graphql.relay_node import DatasourceRelayNode
 from apps.payroll.services.metrics.user import UserMetricsProvider
 from apps.users.models import User
+from apps.users.services.problems.user import get_user_problems
 from .user_metrics import UserMetricsType
 
 
 class UserType(DjangoObjectType):
     metrics = graphene.Field(UserMetricsType)
+    problems = graphene.List(graphene.String)
 
     class Meta:
         model = User
@@ -21,3 +23,6 @@ class UserType(DjangoObjectType):
     def resolve_metrics(self, info, **kwargs):
         provider = UserMetricsProvider()
         return provider.get_metrics(self)
+
+    def resolve_problems(self, info, **kwargs):
+        return get_user_problems(self)
