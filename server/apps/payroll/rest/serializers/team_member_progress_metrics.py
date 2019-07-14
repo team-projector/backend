ac51c068c53e9b4de9ff570cf7c2ab2208238217
Stanislav Lapshin <staslapshin88@gmail.com>
2019-07-14 21:02:41 +0300

    [#186] Fix tests

--- server/apps/payroll/rest/serializers/team_member_progress_metrics.py
+++ server/apps/payroll/rest/serializers/team_member_progress_metrics.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 
+from apps.users.models import User
 from .user_progress_metrics import UserProgressMetricsSerializer
 
 
 class TeamMemberProgressMetricsSerializer(serializers.Serializer):
-    user = serializers.IntegerField()
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
     metrics = UserProgressMetricsSerializer(many=True)
