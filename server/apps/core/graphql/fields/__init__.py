fa14122ec6d204a18f07f2a05db469bb08e0b01d
Stanislav Lapshin <staslapshin88@gmail.com>
2019-07-14 22:00:33 +0300

    [#186] GQL: bitfield

---
+++ server/apps/core/graphql/fields/__init__.py
@@ -0,0 +1,1 @@
+from .bit_field import convert_field_to_float
---
+++ server/apps/core/graphql/fields/bit_field.py
@@ -0,0 +1,32 @@
+import graphene
+from bitfield import BitField as ModelBitField
+from graphene_django.converter import convert_django_field
+
+
+class BitField(graphene.Scalar):
+    @staticmethod
+    def serialize(val):
+        return [
+            v
+            for v, setted in val
+            if setted
+        ]
+
+    @staticmethod
+    def parse_literal(node):
+        # TODO implement
+        return str(node)
+
+    @staticmethod
+    def parse_value(value):
+        # TODO implement
+        return value
+
+
+@convert_django_field.register(ModelBitField)
+def convert_field_to_float(field, registry=None):
+    return graphene.Field(
+        BitField,
+        description=field.help_text,
+        required=not field.null
+    )
