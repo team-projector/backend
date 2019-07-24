90afa05661121e89af35071bedf4365036341c35
Vyacheslav Bondarenko <noszlodno@gmail.com>
2019-07-24 12:23:06 +0300

    [#202] implement logout in graphql

--- server/apps/users/graphql/mutations/main.py
+++ server/apps/users/graphql/mutations/main.py
@@ -1,7 +1,9 @@
-from .login import LoginMutation
 from .gitlab.complete_gitlab_auth import CompleteGitlabAuthMutation
+from .login import LoginMutation
+from .logout import LogoutMutation
 
 
 class AuthMutations:
-    login = LoginMutation.Field()
     complete_gitlab_auth = CompleteGitlabAuthMutation.Field()
+    login = LoginMutation.Field()
+    logout = LogoutMutation.Field()
