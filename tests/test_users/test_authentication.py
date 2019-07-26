0b2189039993a5f4d2313a53f2240a9b8ae92c56
Vyacheslav Bondarenko <noszlodno@gmail.com>
2019-07-26 16:54:56 +0300

    [#206] Remove logout rest api: add login_user test

--- tests/test_users/test_authentication.py
+++ tests/test_users/test_authentication.py
@@ -5,13 +5,37 @@ from pytest import raises
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.settings import api_settings
 
+from apps.users.models import Token
 from apps.users.rest.authentication import TokenAuthentication
 from apps.users.services.token import create_user_token
+from apps.users.services.auth import login_user
+from tests.base import USER_PASSWORD
+
+
+def test_login_user(user):
+    assert TokenAuthentication in api_settings.DEFAULT_AUTHENTICATION_CLASSES
+    assert Token.objects.count() == 0
+
+    token = login_user(user.login, USER_PASSWORD, None)
+
+    assert token is not None
+    assert Token.objects.count() == 1
+
+
+def test_login_user_without_password_or_login(user):
+    with raises(AuthenticationFailed):
+        login_user(user.login, '', None)
+
+    with raises(AuthenticationFailed):
+        login_user('', USER_PASSWORD, None)
+
+
+def test_login_not_existed_user(user):
+    with raises(AuthenticationFailed):
+        login_user(user.login, 'test1234', None)
 
 
 def test_no_auth(user, client):
-    assert TokenAuthentication in api_settings.DEFAULT_AUTHENTICATION_CLASSES
-
     client.user = user
     request = client.get('/')
 
