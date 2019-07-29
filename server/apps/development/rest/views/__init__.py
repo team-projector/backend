2035ba57f66bc412905e45b2953e8a29d2a95b77
Vyacheslav Bondarenko <noszlodno@gmail.com>
2019-07-29 15:17:41 +0300

    [#215] remove gitlab-status rest api

--- server/apps/development/rest/views/__init__.py
+++ server/apps/development/rest/views/__init__.py
@@ -1,7 +1,6 @@
 from .feature_issues import FeatureIssuesViewset
 from .features import FeaturesViewset
 from .gl_issue_status import GitlabIssueStatusView
-from .gl_status import GitlabStatusView
 from .gl_webhook import gl_webhook
 from .issues import IssuesViewset
 from .milestone_features import MilestoneFeaturesViewset
