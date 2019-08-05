adce658f876d2187a43dfa15ce9f9005d3d7f603
Vyacheslav Bondarenko <noszlodno@gmail.com>
2019-08-05 09:55:54 +0300

    [#225] Milestones for project

--- server/apps/development/graphql/types/project.py
+++ server/apps/development/graphql/types/project.py
@@ -1,11 +1,22 @@
+from apps.core.graphql.connection_fields import DataSourceConnectionField
 from apps.core.graphql.connections import DataSourceConnection
 from apps.core.graphql.relay_nodes import DatasourceRelayNode
 from apps.core.graphql.types import BaseDjangoObjectType
 from apps.development.models import Project
+from apps.development.graphql.filters import MilestonesFilterSet
 from apps.development.graphql.types.interfaces import MilestoneOwner
+from apps.development.graphql.types.milestone import MilestoneType
 
 
 class ProjectType(BaseDjangoObjectType):
+    milestones = DataSourceConnectionField(
+        MilestoneType,
+        filterset_class=MilestonesFilterSet
+    )
+
+    def resolve_milestones(self, info, **kwargs):
+        return self.group.milestones
+
     class Meta:
         model = Project
         interfaces = (DatasourceRelayNode, MilestoneOwner)
