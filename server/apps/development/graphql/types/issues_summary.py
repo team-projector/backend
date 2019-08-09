f691b3779653734ce75cb616d1a0338b676b47be
Vyacheslav Bondarenko <noszlodno@gmail.com>
2019-08-09 09:01:03 +0300

    [230] Summary for issues refator

--- server/apps/development/graphql/types/issues_summary.py
+++ server/apps/development/graphql/types/issues_summary.py
@@ -4,8 +4,9 @@ from .issues_project_summary import IssuesProjectSummary
 
 
 class IssuesSummaryType(graphene.ObjectType):
-    issues_count = graphene.Int()
+    count = graphene.Int()
     opened_count = graphene.Int()
+    closed_count = graphene.Int()
     time_spent = graphene.Int()
     problems_count = graphene.Int()
     projects = graphene.List(IssuesProjectSummary)
