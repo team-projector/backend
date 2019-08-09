3abd8a52dfdbba2b82d02a5b6e814d6b7446be1a
Vyacheslav Bondarenko <noszlodno@gmail.com>
2019-08-09 11:34:38 +0300

    [235] Remove developments REST: milestones sync

---
+++ server/apps/development/graphql/mutations/milestones.py
@@ -0,0 +1,35 @@
+import graphene
+from rest_framework.generics import get_object_or_404
+
+from apps.core.graphql.mutations import BaseMutation
+from apps.development.graphql.types import MilestoneType
+from apps.development.models import Milestone, Project, ProjectGroup
+from apps.development.tasks import (
+    sync_project_milestone, sync_group_milestone
+)
+
+
+class SyncMilestoneMutation(BaseMutation):
+    class Arguments:
+        id = graphene.ID()
+
+    milestone = graphene.Field(MilestoneType)
+
+    @classmethod
+    def do_mutate(cls, root, info, id):
+        milestone = get_object_or_404(
+            Milestone.objects.all(),
+            pk=id
+        )
+
+        if milestone.content_type.model_class() == Project:
+            sync_project_milestone.delay(
+                milestone.owner.gl_id,
+                milestone.gl_id
+            )
+        elif milestone.content_type.model_class() == ProjectGroup:
+            sync_group_milestone.delay(
+                milestone.owner.gl_id, milestone.gl_id
+            )
+
+        return SyncMilestoneMutation(milestone=milestone)
