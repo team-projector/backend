613053cf36f01f1526a631b7d284f53a71680b15
Stanislav Lapshin <staslapshin88@gmail.com>
2019-08-23 14:53:48 +0300

    [#] flake8 fix

--- server/apps/payroll/admin/work_break.py
+++ server/apps/payroll/admin/work_break.py
@@ -1,25 +1,8 @@
 from django.contrib import admin
-from django.contrib.admin import helpers
-from django.core.exceptions import ObjectDoesNotExist
-from django.db import transaction
-from django.db.models.fields.related import OneToOneRel
-from django.shortcuts import redirect, render
-from django.utils.html import mark_safe
-from django.urls import path, reverse
 
 from apps.core.admin.base import BaseModelAdmin
-from apps.payroll.admin.forms import GenerateSalaryForm
-from apps.payroll.services.salary.calculator import SalaryCalculator
-from apps.payroll.services.salary.notifications import is_payed
-from apps.payroll.tasks import send_salary_report
 from apps.users.admin.filters import UserFilter
-from .filters import HasSalaryFilter
-from ..models import (
-    Bonus, Payment, Payroll, Penalty, Salary, SpentTime, WorkBreak
-)
-
-
-
+from ..models import WorkBreak
 
 
 @admin.register(WorkBreak)
