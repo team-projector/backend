21e715254ed7a2cb83422fb67cebc5c77b904bea
Vyacheslav Bondarenko <noszlodno@gmail.com>
2019-08-26 21:10:47 +0300

    [255] Split 'tasks.py' to separate modules (like models)

---
+++ server/apps/development/tasks/__init__.py
@@ -0,0 +1,13 @@
+from .issues import sync_issues, sync_project_issue, sync_project_issues
+from .main import sync
+from .merge_requests import (
+    sync_merge_requests, sync_project_merge_request, sync_project_merge_requests
+)
+from .milestones import (
+    load_project_milestones, load_project_group_milestones,
+    sync_group_milestone, sync_groups_milestones, sync_project_milestone,
+    sync_projects_milestones
+)
+from .projects import sync_project
+from .project_groups import sync_project_group
+from .users import sync_user
---
+++ server/apps/development/tasks/project_groups.py
@@ -0,0 +1,20 @@
+from apps.core.activity.verbs import ACTION_GITLAB_CALL_API
+from apps.core.gitlab import get_gitlab_client
+from apps.core.tasks import add_action
+from celery_app import app
+from ..models import ProjectGroup
+from ..services.gitlab.groups import load_single_group
+
+
+@app.task
+def sync_project_group(gl_id: int) -> None:
+    gl = get_gitlab_client()
+    gl_group = gl.groups.get(id=gl_id)
+
+    add_action.delay(verb=ACTION_GITLAB_CALL_API)
+
+    parent = None
+    if gl_group.parent_id:
+        parent = ProjectGroup.objects.get(gl_id=gl_group.parent_id)
+
+    load_single_group(gl_group, parent)
