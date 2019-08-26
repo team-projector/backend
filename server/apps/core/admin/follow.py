1239c154d547a0ed08760445c74badf1727908f6
Vyacheslav Bondarenko <noszlodno@gmail.com>
2019-08-26 22:10:59 +0300

    [254] /admin/actstream/follow/ - autocomplete filter by user

---
+++ server/apps/core/admin/follow.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from actstream.models import Follow
+
+from apps.core.admin.base import BaseModelAdmin
+
+admin.site.unregister(Follow)
+
+
+@admin.register(Follow)
+class FollowAdmin(BaseModelAdmin):
+    list_display = ('__str__', 'user', 'follow_object', 'actor_only', 'started')
+    list_editable = ('user',)
+    list_filter = ('user', 'started',)
+    raw_id_fields = ('user', 'content_type')
