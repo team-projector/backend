218f540d96af34de01b80986933414b1940ce637
snowman <noszlodno@gmail.com>
2019-09-12 23:46:33 +0300

    [289] Milestones only for PM

--- server/apps/development/services/allowed/milestones.py
+++ server/apps/development/services/allowed/milestones.py
@@ -1,14 +1,40 @@
-from django.core.exceptions import PermissionDenied
-from django.db.models import QuerySet
+from django.db.models import Q, QuerySet
 
+from apps.development.models import ProjectGroup, ProjectMember, Milestone
 from apps.users.models import User
 
 
 def filter_allowed_for_user(queryset: QuerySet,
                             user: User) -> QuerySet:
-    if not user.roles.project_manager:
-        raise PermissionDenied(
-            'Only project managers can view project resources'
+    milestones = Milestone.objects.filter(
+        project__members__user=user,
+        project__members__role=ProjectMember.ROLE.project_manager
+    )
+
+    def fill_milestones(groups):
+        nonlocal milestones
+
+        milestones_on_level = Milestone.objects.filter(
+            Q(project_group__in=groups) |
+            Q(project__group__in=groups)
         )
+
+        milestones = milestones | milestones_on_level  # noqa:WPS442
+
+        children_groups = ProjectGroup.objects.filter(
+            parent__in=groups
+        )
+
+        if children_groups:
+            fill_milestones(children_groups)
+
+    groups = ProjectGroup.objects.filter(
+        members__user=user,
+        members__role=ProjectMember.ROLE.project_manager
+    )
+
+    fill_milestones(groups)
+
+    queryset = queryset.intersection(milestones)
 
     return queryset
--- tests/test_development/test_gql_milestones.py
+++ tests/test_development/test_gql_milestones.py
@@ -1,15 +1,20 @@
-from pytest import raises
-from django.core.exceptions import PermissionDenied
-
 from apps.development.graphql.types.milestone import MilestoneType
-from apps.development.models import Milestone
-from tests.test_development.factories import ProjectMilestoneFactory
+from apps.development.models import Milestone, ProjectMember
+from tests.test_development.factories import (
+    ProjectFactory, ProjectMilestoneFactory, ProjectMemberFactory
+)
 from tests.test_development.factories_gitlab import AttrDict
 
 
 def test_milestones(user, client):
-    user.roles.project_manager = True
-    user.save()
+    project = ProjectFactory.create()
+    ProjectMemberFactory.create(
+        user=user,
+        role=ProjectMember.ROLE.project_manager,
+        owner=project
+    )
+
+    milestone = ProjectMilestoneFactory.create(owner=project)
 
     client.user = user
     info = AttrDict({'context': client})
@@ -20,16 +25,5 @@ def test_milestones(user, client):
         Milestone.objects.all(), info
     )
 
-    assert milestones.count() == 5
-
-
-def test_milestones_not_pm(user, client):
-    client.user = user
-    info = AttrDict({'context': client})
-
-    ProjectMilestoneFactory.create_batch(5)
-
-    with raises(PermissionDenied):
-        MilestoneType().get_queryset(
-            Milestone.objects.all(), info
-        )
+    assert milestones.count() == 1
+    assert milestones.first() == milestone
