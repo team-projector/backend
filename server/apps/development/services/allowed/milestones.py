51da0fd387d1121198140c9b39959c08d8d79d4e
snowman <noszlodno@gmail.com>
2019-09-17 14:04:23 +0300

    [289] Milestones only for PM: separate func

--- server/apps/development/services/allowed/milestones.py
+++ server/apps/development/services/allowed/milestones.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import PermissionDenied
 from django.db.models import Q, QuerySet
 
-from apps.development.models import ProjectGroup, ProjectMember, Milestone
+from apps.development.models import Milestone, ProjectGroup, ProjectMember
 from apps.users.models import User
 
 
@@ -9,29 +9,17 @@ def filter_allowed_for_user(queryset: QuerySet,
                             user: User) -> QuerySet:
     members = get_members(user)
 
-    milestones = Milestone.objects.filter(project__members__in=members)
+    project_milestones = Milestone.objects.filter(
+        project__members__in=members
+    ).values('id')
 
-    def fill_milestones(groups: QuerySet) -> None:
-        nonlocal milestones
-
-        milestones_on_level = Milestone.objects.filter(
-            Q(project_group__in=groups) | Q(project__group__in=groups)
-        )
-
-        milestones = milestones | milestones_on_level  # WPS442
-
-        children_groups = ProjectGroup.objects.filter(
-            parent__in=groups
-        )
-
-        if children_groups:
-            fill_milestones(children_groups)
+    milestones_ids = [item.get('id') for item in project_milestones]
 
     groups = ProjectGroup.objects.filter(members__in=members)
 
-    fill_milestones(groups)
+    milestones_ids = get_group_milestones(groups, milestones_ids)
 
-    return queryset.filter(id__in=milestones)
+    return queryset.filter(id__in=milestones_ids)
 
 
 def get_members(user: User) -> list:
@@ -46,3 +34,22 @@ def get_members(user: User) -> list:
         )
 
     return list(members)
+
+
+def get_group_milestones(groups: QuerySet,
+                         milestones_ids: list) -> list:
+    milestones_on_level = Milestone.objects.filter(
+        Q(project_group__in=groups) | Q(project__group__in=groups)
+    ).values('id')
+
+    for item in milestones_on_level:
+        milestones_ids.append(item.get('id'))
+
+    children_groups = ProjectGroup.objects.filter(
+        parent__in=groups
+    )
+
+    if children_groups:
+        get_group_milestones(children_groups, milestones_ids)
+
+    return milestones_ids
