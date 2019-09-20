925a40d5fd08b19b3c1eb3b99f93e321914a8fee
snowman <noszlodno@gmail.com>
2019-09-20 15:26:52 +0300

    [293] test coverage: admin sync

---
+++ tests/test_development/test_admin_issue.py
@@ -0,0 +1,46 @@
+from apps.development.models.issue import Issue, STATE_CLOSED, STATE_OPENED
+from tests.base import model_admin
+from tests.test_development.checkers_gitlab import check_issue
+from tests.test_development.factories import IssueFactory, ProjectFactory
+from tests.test_development.factories_gitlab import (
+    AttrDict, GlIssueFactory, GlProjectFactory, GlTimeStats, GlUserFactory,
+)
+
+
+def test_sync_handler(db, gl_mocker):
+    ma_issue = model_admin(Issue)
+
+    gl_mocker.registry_get('/user', GlUserFactory())
+
+    gl_project = AttrDict(GlProjectFactory())
+    project = ProjectFactory.create(gl_id=gl_project.id)
+    gl_mocker.registry_get(f'/projects/{gl_project.id}', gl_project)
+
+    gl_assignee = AttrDict(GlUserFactory())
+    gl_mocker.registry_get(f'/users/{gl_assignee.id}', gl_assignee)
+
+    gl_issue = AttrDict(GlIssueFactory(
+        project_id=gl_project.id, assignee=gl_assignee, state=STATE_CLOSED
+    ))
+    issue = IssueFactory.create(
+        gl_id=gl_issue.id, gl_iid=gl_issue.iid, project=project, state=STATE_OPENED
+    )
+    gl_mocker.registry_get(f'/projects/{gl_project.id}/issues/{gl_issue.iid}', gl_issue)
+
+    _registry_issue(gl_mocker, gl_project, gl_issue)
+
+    ma_issue.sync_handler(issue)
+
+    issue = Issue.objects.first()
+
+    check_issue(issue, gl_issue)
+
+
+def _registry_issue(gl_mocker, gl_project, gl_issue) -> None:
+    gl_mocker.registry_get(f'/projects/{gl_project.id}/issues', [gl_issue])
+    gl_mocker.registry_get(f'/projects/{gl_project.id}/issues/{gl_issue.iid}', gl_issue)
+    gl_mocker.registry_get(f'/projects/{gl_project.id}/issues/{gl_issue.iid}/time_stats', GlTimeStats())
+    gl_mocker.registry_get(f'/projects/{gl_project.id}/issues/{gl_issue.iid}/closed_by', [])
+    gl_mocker.registry_get(f'/projects/{gl_project.id}/labels', [])
+    gl_mocker.registry_get(f'/projects/{gl_project.id}/issues/{gl_issue.iid}/notes', [])
+    gl_mocker.registry_get(f'/projects/{gl_project.id}/issues/{gl_issue.iid}/participants', [])
---
+++ tests/test_development/test_admin_milestone.py
@@ -0,0 +1,53 @@
+from apps.development.models import Milestone
+from tests.base import model_admin
+from tests.test_development.checkers_gitlab import check_milestone
+from tests.test_development.factories import (
+    ProjectGroupFactory, ProjectFactory, ProjectMilestoneFactory,
+    ProjectGroupMilestoneFactory
+)
+from tests.test_development.factories_gitlab import (
+    AttrDict, GlGroupFactory, GlProjectFactory, GlProjectMilestoneFactory,
+    GlUserFactory,
+)
+
+
+def test_sync_group_milestone(db, gl_mocker):
+    ma_milestone = model_admin(Milestone)
+
+    gl_mocker.registry_get('/user', GlUserFactory())
+
+    gl_group = AttrDict(GlGroupFactory())
+    group = ProjectGroupFactory.create(gl_id=gl_group.id)
+    gl_mocker.registry_get(f'/groups/{gl_group.id}', gl_group)
+
+    gl_milestone = AttrDict(GlProjectMilestoneFactory())
+    milestone = ProjectGroupMilestoneFactory.create(gl_id=gl_milestone.id, owner=group)
+    gl_mocker.registry_get(f'/groups/{gl_group.id}/milestones/{gl_milestone.id}',
+                           gl_milestone)
+
+    ma_milestone.sync_handler(milestone)
+
+    milestone = Milestone.objects.get(gl_id=gl_milestone.id)
+
+    check_milestone(milestone, gl_milestone, group)
+
+
+def test_sync_project_milestone(db, gl_mocker):
+    ma_milestone = model_admin(Milestone)
+
+    gl_mocker.registry_get('/user', GlUserFactory())
+
+    gl_project = AttrDict(GlProjectFactory())
+    project = ProjectFactory.create(gl_id=gl_project.id)
+    gl_mocker.registry_get(f'/projects/{gl_project.id}', gl_project)
+
+    gl_milestone = AttrDict(GlProjectMilestoneFactory())
+    milestone = ProjectMilestoneFactory.create(gl_id=gl_milestone.id, owner=project)
+    gl_mocker.registry_get(f'/projects/{gl_project.id}/milestones'
+                           f'/{gl_milestone.id}', gl_milestone)
+
+    ma_milestone.sync_handler(milestone)
+
+    milestone = Milestone.objects.get(gl_id=gl_milestone.id)
+
+    check_milestone(milestone, gl_milestone, project)
