7c1abd393f1168e7e97fa74e429ec502b07f7694
Viacheslav Riazanov <riazanovslv@gmail.com>
2019-09-24 16:25:21 +0300

    [#283] remove graphql optimizer, fix prefetch

--- tests/test_development/test_gql_issues_fetch.py
+++ tests/test_development/test_gql_issues_fetch.py
@@ -1,42 +1,56 @@
+from graphql.language.ast import Field, Name, SelectionSet
 from django.db import connection
 from django.test import override_settings
 
+from apps.development.graphql.types.issue import IssueType
+from apps.development.models import Issue
 from tests.test_development.factories import IssueFactory
+from tests.test_development.factories_gitlab import AttrDict
 
 
 @override_settings(DEBUG=True)
-def test_select_related_user(admin_user, gql_client_authenticated):
-    IssueFactory.create(user=admin_user)
-    query1 = '''
-            {
-              allIssues {
-                edges {
-                  node {
-                    id
-                  }
-                }
-              }
-            }
-            '''
+def test_select_related_user(user, client):
+    IssueFactory.create(user=user)
 
-    query2 = '''
-            {
-              allIssues {
-                edges {
-                  node {
-                    id
-                    user {
-                      id
-                    }
-                  }
-                }
-              }
-            }
-            '''
+    client.user = user
+    info = AttrDict({
+        'context': client,
+        'field_asts': [{}],
+        'fragments': {},
+    })
 
-    count_before = len(connection.queries)
-    gql_client_authenticated.execute(query1)
-    for_request = len(connection.queries) - count_before
+    query_count_before = len(connection.queries)
 
-    gql_client_authenticated.execute(query2)
-    assert len(connection.queries) == count_before + 2 * for_request
+    issues = IssueType().get_queryset(
+        Issue.objects.all(), info
+    )
+
+    assert issues.first().user == user
+
+    query_count_after = len(connection.queries)
+    assert query_count_after - query_count_before == 3
+
+    user_field = _create_field('user')
+    node = _create_field('node', user_field)
+    edges = _create_field('edges', node)
+    info.field_asts = [_create_field('allIssues', edges)]
+
+    query_count_before = len(connection.queries)
+
+    issues = IssueType().get_queryset(
+        Issue.objects.all(), info
+    )
+
+    assert issues.first().user == user
+
+    query_count_after = len(connection.queries)
+    assert query_count_after - query_count_before == 2
+
+
+def _create_field(name, selection=None):
+    field = Field(name=Name(value=name))
+
+    if selection:
+        field.selection_set = SelectionSet(selections=[selection])
+
+    return field
