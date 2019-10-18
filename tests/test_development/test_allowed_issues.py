100f04c420fa125503cd8a3bb9c4f0fd384fd218
snowman <noszlodno@gmail.com>
2019-10-18 14:15:23 +0300

    [352] Project manager can't see issues without user: remove duplicates

--- tests/test_development/test_allowed_issues.py
+++ tests/test_development/test_allowed_issues.py
@@ -1,6 +1,11 @@
 from apps.development.models import Issue, TeamMember
+from apps.development.models.project_member import PROJECT_MEMBER_ROLES
 from tests.test_development.factories import (
-    IssueFactory, TeamFactory, TeamMemberFactory
+    IssueFactory,
+    ProjectFactory,
+    ProjectMemberFactory,
+    TeamFactory,
+    TeamMemberFactory,
 )
 from tests.test_users.factories import UserFactory
 
@@ -84,3 +89,148 @@ def test_by_team_watcher(user):
     allowed = Issue.objects.allowed_for_user(watcher)
 
     assert allowed.count() == 4
+
+
+def test_by_project_manager(db):
+    project_1 = ProjectFactory.create()
+    pm_1 = ProjectMemberFactory.create(
+        role=PROJECT_MEMBER_ROLES.project_manager,
+        owner=project_1,
+    )
+
+    issue_1 = IssueFactory.create(project=project_1)
+    issue_2 = IssueFactory.create(project=project_1)
+
+    project_2 = ProjectFactory.create()
+    pm_2 = ProjectMemberFactory.create(
+        role=PROJECT_MEMBER_ROLES.project_manager,
+        owner=project_2,
+    )
+
+    issue_3 = IssueFactory.create(project=project_2)
+    issue_4 = IssueFactory.create(project=project_2)
+
+    project_3 = ProjectFactory.create()
+    pm_3 = ProjectMemberFactory.create(
+        role=PROJECT_MEMBER_ROLES.developer,
+        owner=project_3,
+    )
+
+    IssueFactory.create_batch(3, project=project_3)
+
+    project_4 = ProjectFactory.create()
+    pm_4 = ProjectMemberFactory.create(
+        role=PROJECT_MEMBER_ROLES.customer,
+        owner=project_4,
+    )
+
+    IssueFactory.create_batch(3, project=project_4)
+
+    allowed = Issue.objects.allowed_for_user(pm_1.user)
+
+    assert allowed.count() == 2
+    assert set(allowed) == {issue_1, issue_2}
+
+    allowed = Issue.objects.allowed_for_user(pm_2.user)
+
+    assert allowed.count() == 2
+    assert set(allowed) == {issue_3, issue_4}
+
+    allowed = Issue.objects.allowed_for_user(pm_3.user)
+
+    assert allowed.count() == 0
+
+    allowed = Issue.objects.allowed_for_user(pm_4.user)
+
+    assert allowed.count() == 0
+
+
+def test_project_members_team_members(db):
+    user_pm = UserFactory.create()
+    user_lead = UserFactory.create()
+    user_dev = UserFactory.create()
+
+    team = TeamFactory.create()
+
+    TeamMemberFactory.create(
+        user=user_pm,
+        team=team,
+        roles=TeamMember.roles.watcher
+    )
+    TeamMemberFactory.create(
+        user=user_lead,
+        team=team,
+        roles=TeamMember.roles.leader
+    )
+    TeamMemberFactory.create(
+        user=user_dev,
+        team=team,
+        roles=TeamMember.roles.developer
+    )
+
+    project_1 = ProjectFactory.create()
+    ProjectMemberFactory.create(
+        user=user_pm,
+        role=PROJECT_MEMBER_ROLES.project_manager,
+        owner=project_1,
+    )
+    ProjectMemberFactory.create(
+        user=user_lead,
+        role=PROJECT_MEMBER_ROLES.developer,
+        owner=project_1,
+    )
+    ProjectMemberFactory.create(
+        user=user_dev,
+        role=PROJECT_MEMBER_ROLES.developer,
+        owner=project_1,
+    )
+
+    issue_1 = IssueFactory.create(project=project_1, user=user_pm)
+    issue_2 = IssueFactory.create(project=project_1, user=user_lead)
+    issue_3 = IssueFactory.create(project=project_1, user=user_dev)
+    issue_4 = IssueFactory.create(project=project_1, user=None)
+
+    project_2 = ProjectFactory.create()
+    ProjectMemberFactory.create(
+        user=user_pm,
+        role=PROJECT_MEMBER_ROLES.project_manager,
+        owner=project_2,
+    )
+    ProjectMemberFactory.create(
+        user=user_lead,
+        role=PROJECT_MEMBER_ROLES.developer,
+        owner=project_2,
+    )
+    ProjectMemberFactory.create(
+        user=user_dev,
+        role=PROJECT_MEMBER_ROLES.developer,
+        owner=project_2,
+    )
+
+    issue_5 = IssueFactory.create(project=project_2, user=user_dev)
+    issue_6 = IssueFactory.create(project=project_2, user=user_dev)
+    issue_7 = IssueFactory.create(project=project_2, user=None)
+
+    project_3 = ProjectFactory.create()
+    ProjectMemberFactory.create(
+        user=user_lead,
+        role=PROJECT_MEMBER_ROLES.developer,
+        owner=project_3,
+    )
+    IssueFactory.create_batch(10, project=project_3)
+
+    allowed = Issue.objects.allowed_for_user(user_pm)
+
+    assert allowed.count() == 7
+    assert set(allowed) == {issue_1, issue_2, issue_3, issue_4, issue_5,
+                            issue_6, issue_7}
+
+    allowed = Issue.objects.allowed_for_user(user_lead)
+
+    assert allowed.count() == 5
+    assert set(allowed) == {issue_1, issue_2, issue_3, issue_5, issue_6}
+
+    allowed = Issue.objects.allowed_for_user(user_dev)
+
+    assert allowed.count() == 3
+    assert set(allowed) == {issue_3, issue_5, issue_6}
