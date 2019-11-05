52ca9b21d097697507ad866287b00ab4e9a885d3
snowman <noszlodno@gmail.com>
2019-11-05 11:37:55 +0300

    [363] Adjust commands in development app

--- server/apps/development/management/commands/load_merge_requests_from_gitlab.py
+++ server/apps/development/management/commands/load_merge_requests_from_gitlab.py
@@ -2,9 +2,11 @@
 
 from django.core.management.base import BaseCommand
 
-from apps.development.services.merge_request.gitlab import load_merge_requests
+from apps.development.services.merge_request.gl.manager import (
+    MergeRequestGlManager,
+)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        load_merge_requests(True)
+        MergeRequestGlManager().sync_merge_requests(True)
