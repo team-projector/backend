21e8bb2bd3b999c41ebb5d2ebde49301e8f32d74
snowman <noszlodno@gmail.com>
2019-11-05 08:44:52 +0300

    [361] Split "test_tasks" test

---
+++ tests/test_development/gl/tasks/test_users.py
@@ -0,0 +1,23 @@
+from django.test import override_settings
+
+from apps.development.tasks import sync_user_task
+from apps.users.models import User
+from tests.test_development.checkers_gitlab import check_user
+from tests.test_development.factories_gitlab import (
+    AttrDict,
+    GlUserFactory,
+)
+
+
+@override_settings(GITLAB_TOKEN='GITLAB_TOKEN')
+def test_sync_user_task(db, gl_mocker):
+    gl_user = AttrDict(GlUserFactory())
+
+    gl_mocker.registry_get('/user', GlUserFactory())
+    gl_mocker.registry_get(f'/users/{gl_user.id}', gl_user)
+
+    sync_user_task(gl_user.id)
+
+    user = User.objects.get(gl_id=gl_user.id)
+
+    check_user(user, gl_user)
