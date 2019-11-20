72bf1ce9dad753722dd8d92e1981954abea6c94f
Stanislav Lapshin <staslapshin88@gmail.com>
2019-11-20 21:06:17 +0300

    [#] Add pytests linters

--- tests/test_core/test_camel_cased_ordering_filter.py
+++ tests/test_core/test_camel_cased_ordering_filter.py
@@ -1,27 +1,34 @@
-from apps.core.graphql.filters.mixins import CamelCasedOrderingMixin
+# -*- coding: utf-8 -*-
+
 from django_filters import OrderingFilter as BaseOrderingFilter
 
+from apps.core.graphql.filters.mixins import CamelCasedOrderingMixin
 
-class OrderingFilter(CamelCasedOrderingMixin,
-                     BaseOrderingFilter):
-    pass
+
+class OrderingFilter(
+    CamelCasedOrderingMixin,
+    BaseOrderingFilter,
+):
+    """Test ordering filter."""
 
 
 def test_camel_case():
-    f = OrderingFilter(fields=(
+    """Test camel case filtering."""
+    filt = OrderingFilter(fields=(
         ('field_i18n', 'field_i18n'),
         'snakes_on_a__plane',
     ))
 
-    assert f.get_ordering_value('fieldI18n') == 'field_i18n'
-    assert f.get_ordering_value('snakesOnA__plane') == 'snakes_on_a__plane'
+    assert filt.get_ordering_value('fieldI18n') == 'field_i18n'
+    assert filt.get_ordering_value('snakesOnA__plane') == 'snakes_on_a__plane'
 
 
 def test_camel_case_input_ad_dict():
-    f = OrderingFilter(fields={
+    """Test camel case filtering with dict fields."""
+    filt = OrderingFilter(fields={
         'due_date': 'due_date',
-        'user__due_date': 'user__due_date'
+        'user__due_date': 'user__due_date',
     })
 
-    assert f.get_ordering_value('dueDate') == 'due_date'
-    assert f.get_ordering_value('user__dueDate') == 'user__due_date'
+    assert filt.get_ordering_value('dueDate') == 'due_date'
+    assert filt.get_ordering_value('user__dueDate') == 'user__due_date'
