8338be08967ef4659322d2ab8c656b5105301b79
Andrey Surovov <andrey.surovov@gmail.com>
2019-12-27 08:02:47 +0000

    Resolve "Use ManifestStaticFilesStorage"

--- tests/fixtures/settings.py
+++ tests/fixtures/settings.py
@@ -3,15 +3,13 @@
 import pytest
 
 
-@pytest.fixture(autouse=True)  # type: ignore
-def _media_root(settings, tmpdir_factory) -> None:
-    """Forces django to save media files into temp folder."""
-    settings.MEDIA_ROOT = tmpdir_factory.mktemp('media', numbered=True)
-
-
 @pytest.fixture(autouse=True)
-def _password_hashers(settings):
-    """Forces django to use fast password hashers for tests."""
+def _django_settings(settings, tmpdir_factory) -> None:
+    """Forces django test settings."""
+    settings.MEDIA_ROOT = tmpdir_factory.mktemp('media', numbered=True)
     settings.PASSWORD_HASHERS = [
         'django.contrib.auth.hashers.MD5PasswordHasher',
     ]
+    settings.STATICFILES_STORAGE = (
+        'django.contrib.staticfiles.storage.StaticFilesStorage'
+    )
