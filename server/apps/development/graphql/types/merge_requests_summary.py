c7978dbd3995eccb9d907f3dd157f014099c04c4
Viacheslav Riazanov <riazanovslv@gmail.com>
2020-01-01 19:20:55 +0000

    Resolve "Refactor merge requests ghl"

--- server/apps/development/graphql/types/merge_requests_summary.py
+++ server/apps/development/graphql/types/merge_requests_summary.py
@@ -2,9 +2,17 @@
 
 import graphene
 
+from apps.core.graphql.security.mixins.node import AuthNode
+from apps.core.graphql.security.permissions import AllowAuthenticated
 
-class MergeRequestsSummaryType(graphene.ObjectType):
+
+class MergeRequestsSummaryType(
+    AuthNode,
+    graphene.ObjectType,
+):
     """Merge requests summary type."""
+
+    permission_classes = (AllowAuthenticated,)
 
     count = graphene.Int()
     opened_count = graphene.Int()
---
+++ tests/test_development/test_ghl/test_merge_requests/test_queries/test_metrics.py
@@ -0,0 +1,15 @@
+from apps.core.utils.time import seconds
+from apps.development.graphql.types import MergeRequestType
+from tests.test_development.factories import MergeRequestFactory
+
+
+def test_metrics(user):
+    """Test merge request metrics type."""
+    merge_request = MergeRequestFactory.create(
+        user=user,
+        total_time_spent=seconds(hours=1),
+        time_estimate=seconds(hours=2)
+    )
+
+    metrics = MergeRequestType.resolve_metrics(merge_request, None)
+    assert metrics.remains == seconds(hours=1)
