99c834b385b108bfaca197eddaeff07debfd7678
Andrey Surovov <andrey.surovov@gmail.com>
2020-02-12 12:09:32 +0000

    Resolve "Update ticket state GQL"

--- tests/test_development/factories/ticket.py
+++ tests/test_development/factories/ticket.py
@@ -1,6 +1,7 @@
 import factory
 
 from apps.development.models import Ticket
+from apps.development.models.ticket import STATE_CREATED
 from tests.test_development.factories.project_group_milestone import (
     ProjectGroupMilestoneFactory,
 )
@@ -10,6 +11,7 @@ class TicketFactory(factory.django.DjangoModelFactory):
     title = factory.Faker("text", max_nb_chars=200)
     url = factory.Sequence(lambda seq: f"https://team-projector-{seq}.com")
     milestone = factory.SubFactory(ProjectGroupMilestoneFactory)
+    state = STATE_CREATED
 
     class Meta:
         model = Ticket
