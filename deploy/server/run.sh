cd508ce8cb0005117d8ee7f9ed9d7f6e623289cb
Stanislav Lapshin <staslapshin88@gmail.com>
2020-05-22 15:02:15 +0300

    [#] Auto uwsgi processes count

--- deploy/server/run.sh
+++ deploy/server/run.sh
@@ -1,6 +1,14 @@
-#! /bin/sh
+#! /bin/bash
 
-./manage.py migrate \
-  && uwsgi --ini deploy/server/uwsgi.ini --processes $UWSGI_PROCESSES_COUNT &
+set -o errexit
+
+./manage.py migrate
 
 nginx
+
+_UWSGI_OPTS="--ini uwsgi.ini"
+if [ "${UWSGI_PROCESSES_AUTO:-}" == "1" ]; then
+  _UWSGI_OPTS="${_UWSGI_OPTS} --processes $(nproc)"
+fi
+
+uwsgi $_UWSGI_OPTS
