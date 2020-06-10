0ca2b2f44e1ea5fa5451ff8e801fda058ae06a7e
Andrey Surovov <andrey.surovov@gmail.com>
2020-06-10 12:17:53 +0000

    Resolve "ConnectionError: HTTPSConnectionPool(host='gitlab.com', port=443): Max retries exceeded with url: /api/v4/projects..."

--- server/apps/development/services/project/gl/provider.py
+++ server/apps/development/services/project/gl/provider.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 
+import logging
 from typing import Optional
 
 from gitlab.v4 import objects as gl
 from rest_framework import status
+from urllib3.exceptions import NewConnectionError
 
 from apps.core.activity.decorators import gitlab_api_call
 from apps.core.gitlab.provider import BaseGlProvider
 from apps.development.models import Project
+
+logger = logging.getLogger(__name__)
 
 
 class ProjectGlProvider(BaseGlProvider):
@@ -18,6 +22,8 @@ class ProjectGlProvider(BaseGlProvider):
         """Load project from gitlab."""
         try:
             return self.gl_client.projects.get(id=project.gl_id)
+        except NewConnectionError:
+            logger.exception("Connection timed out.")
         except gl.GitlabGetError as error:
             if error.response_code == status.HTTP_404_NOT_FOUND:
                 project.is_active = False
