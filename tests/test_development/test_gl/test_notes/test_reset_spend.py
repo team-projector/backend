1981c7e8e17557f1fc7741a4268e85b03361ecf3
Stanislav Lapshin <staslapshin88@gmail.com>
2020-07-01 19:24:20 +0300

    [#] Add note gl notifacation webhook

--- tests/test_development/test_gl/test_notes/test_reset_spend.py
+++ tests/test_development/test_gl/test_notes/test_reset_spend.py
@@ -7,9 +7,7 @@ from jnt_django_toolbox.helpers.objects import dict2obj
 from apps.core.gitlab import GITLAB_DATETIME_FORMAT
 from apps.development.models import Note
 from apps.development.models.note import NoteType
-from apps.development.services.note.notes_parsers.base import (
-    SPEND_RESET_MESSAGE,
-)
+from apps.development.services.note.gl.parsers.base import SPEND_RESET_MESSAGE
 from tests.test_development.factories import IssueFactory
 
 
