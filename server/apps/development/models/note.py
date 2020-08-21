361ed7db333ce21db49f7c7bb20d8722431fa315
Stanislav Lapshin <staslapshin88@gmail.com>
2020-08-21 14:58:46 +0300

    [#] Update libs

--- server/apps/development/models/note.py
+++ server/apps/development/models/note.py
@@ -2,7 +2,6 @@
 
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.postgres.fields import JSONField
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -51,7 +50,7 @@ class Note(models.Model):
         help_text=_("HT__TYPE"),
     )
 
-    data = JSONField(encoder=DjangoJSONEncoder)  # noqa: WPS110
+    data = models.JSONField(encoder=DjangoJSONEncoder)  # noqa: WPS110
 
     content_object = GenericForeignKey()
 
