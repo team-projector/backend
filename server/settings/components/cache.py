3eeed464a495137102e2fbe1f9dd0168d80cd63d
Stanislav Lapshin <staslapshin88@gmail.com>
2020-09-01 20:36:40 +0300

    [#] Small improvements

--- server/settings/components/cache.py
+++ server/settings/components/cache.py
@@ -5,9 +5,6 @@ from decouple import config
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.memcached.MemcachedCache",
-        "LOCATION": "{0}:{1}".format(
-            config("DJANGO_CACHE_HOST", "memcached"),
-            config("DJANGO_CACHE_PORT", "11211"),
-        ),
+        "LOCATION": config("DJANGO_MEMCACHED", "memcached:11211"),
     },
 }
