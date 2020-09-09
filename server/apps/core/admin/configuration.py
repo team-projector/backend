64a3c10122025736205724384192ec1af89964d0
Andrey Surovov <andrey.surovov@gmail.com>
2020-09-09 18:34:32 +0300

    [#506] Improve default choices

--- server/apps/core/admin/configuration.py
+++ server/apps/core/admin/configuration.py
@@ -2,6 +2,7 @@
 
 import calendar
 from datetime import date, datetime
+from typing import Optional
 
 from constance import LazyConfig
 from constance.admin import Config, ConstanceAdmin
@@ -20,14 +21,14 @@ class ConfigurationAdmin(ConstanceAdmin):
 
     def get_config_value(self, name, options, form, initial):
         """Override from parent."""
-        default, help_text = options[0], options[1]
+        default, help_text = (
+            self._update_default_value(name, options[0]),
+            options[1],
+        )
         source_value = initial.get(name)
 
         if source_value is None:
             source_value = getattr(config, name)
-
-        if default and name in CALENDAR_CONFIG_NAMES:
-            default = tuple(calendar.day_name[day] for day in default)
 
         return {
             "name": name,
@@ -44,6 +45,13 @@ class ConfigurationAdmin(ConstanceAdmin):
             ),
             "is_file": isinstance(form[name].field.widget, forms.FileInput),
         }
+
+    def _update_default_value(self, name, default) -> Optional[object]:
+        """Update defaults for calendar."""
+        if default and name in CALENDAR_CONFIG_NAMES:
+            default = tuple(calendar.day_name[day] for day in default)
+
+        return default
 
 
 admin.site.unregister([Config])
