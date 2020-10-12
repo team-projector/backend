5f9844e32110a310983a92937d4d412b0797113c
Andrey Surovov <andrey.surovov@gmail.com>
2020-10-12 12:18:58 +0300

    Add handle errors to SlackClient for `send data` #525

---
+++ server/apps/core/notifications/slack/decorators.py
@@ -0,0 +1,20 @@
+import logging
+
+from django.utils.decorators import method_decorator
+
+logger = logging.getLogger(__name__)
+
+
+@method_decorator
+def error_handler(method):
+    """Handle any error."""
+
+    def wrapper(*args, **kwargs):  # noqa: WPS430
+        """Help-wrapper for handle error."""
+        try:
+            return method(*args, **kwargs)
+        except Exception as error:
+            logger.error("Failed to send a data to Slack {0}".format(error))
+            return None
+
+    return wrapper
