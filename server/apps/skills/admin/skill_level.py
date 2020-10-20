48cb81da4443160ae95aa994d693db0ffc48cba9
Stanislav Lapshin <sm.lapshin@gmail.com>
2020-10-20 16:56:24 +0300

    Change admin look and feel #552

--- server/apps/skills/admin/skill_level.py
+++ server/apps/skills/admin/skill_level.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from jnt_django_toolbox.admin.decorators import admin_link
 
 from apps.core.admin.base import BaseModelAdmin
-from apps.skills.models import SkillLevel
+from apps.skills.models import SkillGroup, SkillLevel
 
 
 @admin.register(SkillLevel)
@@ -9,5 +10,10 @@ class SkillLevelAdmin(BaseModelAdmin):
     """A class represents skill level model for admin dashboard."""
 
     search_fields = ("skill__title",)
-    list_display = ("description", "level", "group", "skill")
+    list_display = ("description", "level", "group_link", "skill")
     list_filter = ("skill",)
+
+    @admin_link("skill__group")
+    def group_link(self, instance: SkillGroup):
+        """Provides skill group."""
+        return str(instance)
