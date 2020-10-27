81f4d19525a84504143c0512e39319dbf136dd3f
Stanislav Lapshin <sm.lapshin@gmail.com>
2020-10-27 18:07:38 +0300

    Refactor cmd scripts

--- deploy/server/celery_worker.sh
+++ deploy/server/celery_worker.sh
@@ -2,12 +2,14 @@
 
 set -o errexit
 
-_CELERY_OPTS="-A server.celery_app worker -Q $CELERY_QUEUES"
-if [ "${CELERY_CONCURRENCY:-}" != "" ]; then
+_CELERY_OPTS="-A server.celery_app worker"
+if [ "${CELERY_CONCURRENCY:-}" != "" ]
+then
   _CELERY_OPTS="${_CELERY_OPTS} --concurrency ${CELERY_CONCURRENCY}"
 fi
 
-if [ "${CELERY_QUEUES:-}" != "" ]; then
+if [ "${CELERY_QUEUES:-}" != "" ]
+then
   _CELERY_OPTS="${_CELERY_OPTS} -Q ${CELERY_QUEUES}"
 fi
 
