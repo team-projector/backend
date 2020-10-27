ba41e62743de62fedfc4309b1fc4306ebae9438a
Stanislav Lapshin <sm.lapshin@gmail.com>
2020-10-27 17:42:22 +0300

    Add user to Dockerfile #527

--- deploy/server/celery_beat.sh
+++ deploy/server/celery_beat.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-celery -A server.celery_app beat -s /var/run/app/beat_schedule
+celery -A server.celery_app beat \
+        -s /var/run/app/celerybeat.schedule \
+        --pidfile /var/run/app/celerybeat.pid
