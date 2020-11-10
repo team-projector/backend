1d49700f13bebed246805a90b0025d6149202ae0
Andrey Surovov <andrey.surovov@gmail.com>
2020-11-10 16:00:40 +0300

    Refactor permissions for metrics, problems. Add test #584

--- tests/test_development/test_services/test_milestones/test_allowed.py
+++ tests/test_development/test_services/test_milestones/test_allowed.py
@@ -2,7 +2,10 @@ import pytest
 from jnt_django_graphene_toolbox.errors import GraphQLPermissionDenied
 
 from apps.development.models import Milestone
-from apps.development.services.milestone.allowed import filter_allowed_for_user
+from apps.development.services.milestone.allowed import (
+    filter_allowed_for_user,
+    is_project_manager,
+)
 from tests.test_development.factories import (
     ProjectFactory,
     ProjectGroupFactory,
@@ -61,7 +64,7 @@ def test_projects(user, make_project_manager):
         ProjectMilestoneFactory.create(owner=projects[0]),
     }
 
-    ProjectMilestoneFactory.create_batch(10)
+    ProjectMilestoneFactory.create_batch(2)
 
     queryset = filter_allowed_for_user(Milestone.objects.all(), user)
 
@@ -139,7 +142,7 @@ def test_group_with_projects(user, make_project_manager, make_group_manager):
         ProjectMilestoneFactory.create(owner=projects[1]),
     }
 
-    ProjectMilestoneFactory.create_batch(10)
+    ProjectMilestoneFactory.create_batch(2)
 
     queryset = filter_allowed_for_user(Milestone.objects.all(), user)
 
@@ -169,7 +172,7 @@ def test_parent_group_with_groups(
         ProjectMilestoneFactory.create(owner=groups[1]),
     }
 
-    ProjectMilestoneFactory.create_batch(10)
+    ProjectMilestoneFactory.create_batch(2)
 
     queryset = filter_allowed_for_user(Milestone.objects.all(), user)
 
@@ -208,9 +211,38 @@ def test_parent_group_with_groups_and_projects(
         ProjectMilestoneFactory.create(owner=projects[2]),
     }
 
-    ProjectMilestoneFactory.create_batch(10)
+    ProjectMilestoneFactory.create_batch(2)
 
     queryset = filter_allowed_for_user(Milestone.objects.all(), user)
 
     assert queryset.count() == 5
     assert set(queryset) == milestones
+
+
+def test_is_project_manager(
+    user,
+    make_project_manager,
+    make_group_manager,
+):
+    """Test is project manager."""
+    parent_group = ProjectGroupFactory.create()
+    make_group_manager(parent_group, user)
+
+    groups = ProjectGroupFactory.create_batch(2, parent=parent_group)
+
+    projects = [
+        ProjectFactory.create(group=parent_group),
+        ProjectFactory.create(group=groups[0]),
+        ProjectFactory.create(group=groups[1]),
+    ]
+
+    milestones = {
+        ProjectMilestoneFactory.create(owner=groups[0]),
+        ProjectMilestoneFactory.create(owner=groups[1]),
+        ProjectMilestoneFactory.create(owner=projects[0]),
+        ProjectMilestoneFactory.create(owner=projects[1]),
+        ProjectMilestoneFactory.create(owner=projects[2]),
+    }
+
+    for milestone in milestones:
+        assert is_project_manager(user, milestone)
