f62a502814b9653b6eaa159e902cec40db7c0dd0
Andrey Surovov <andrey.surovov@gmail.com>
2020-12-02 14:44:06 +0300

    Add filter by project to milestones #622

---
+++ tests/test_development/test_ghl/test_milestones/test_filters/test_by_project.py
@@ -0,0 +1,57 @@
+import pytest
+
+from apps.development.graphql.filters import MilestonesFilterSet
+from apps.development.models import Milestone
+from tests.test_development.factories import (
+    ProjectFactory,
+    ProjectMilestoneFactory,
+)
+
+
+@pytest.fixture()
+def project(db):
+    """Create project."""
+    return ProjectFactory.create()
+
+
+@pytest.fixture()
+def milestones(db):
+    """Create project milestones."""
+    return ProjectMilestoneFactory.create_batch(4)
+
+
+def test_empty_filter(milestones):
+    """Test not filter."""
+    filter_set = MilestonesFilterSet(
+        {"project": None},
+        queryset=Milestone.objects,
+    )
+
+    assert filter_set.is_valid()
+    assert filter_set.qs.count() == 4
+
+
+def test_filter_by_project_empty(project, milestones):
+    """Test filter by project."""
+    filter_set = MilestonesFilterSet(
+        {"project": project.pk},
+        queryset=Milestone.objects,
+    )
+
+    assert filter_set.is_valid()
+    assert not filter_set.qs.exists()
+
+
+def test_filter_by_project(project, milestones):
+    """Test filter by project."""
+    milestones[2].owner = project
+    milestones[2].save()
+
+    filter_set = MilestonesFilterSet(
+        {"project": project.pk},
+        queryset=Milestone.objects,
+    )
+
+    assert filter_set.is_valid()
+    assert filter_set.qs.count() == 1
+    assert filter_set.qs.first() == milestones[2]
