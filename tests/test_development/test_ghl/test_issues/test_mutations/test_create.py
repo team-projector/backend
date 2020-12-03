736ce5cb3fdb61619c7af7a2e462018ad0ded99b
Andrey Surovov <andrey.surovov@gmail.com>
2020-12-03 12:50:52 +0300

    Fix test #626

--- tests/test_development/test_ghl/test_issues/test_mutations/test_create.py
+++ tests/test_development/test_ghl/test_issues/test_mutations/test_create.py
@@ -67,7 +67,7 @@ def test_raw_query(user, project, ghl_client, gl_mocker, ghl_raw):
         variable_values={
             "title": "Test issue",
             "project": project.pk,
-            "developer": user.pk,
+            "user": user.pk,
             "estimate": estimate,
             "dueDate": str(datetime.now().date()),
             "milestone": None,
