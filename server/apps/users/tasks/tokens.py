7f6df786b3b98815c0ef01b10ac69716caf5efda
Stanislav Lapshin <sm.lapshin@gmail.com>
2020-12-24 16:30:04 +0300

    Fix broken import

--- server/apps/users/tasks/tokens.py
+++ server/apps/users/tasks/tokens.py
@@ -1,8 +1,10 @@
-from apps.users.services.token.clear import clear_tokens
+from apps.core import injector
+from apps.users.services.token import TokenService
 from celery_app import app
 
 
 @app.task
 def clear_expired_tokens_task() -> None:
     """Clears expired tokens."""
-    clear_tokens()
+    token_service = injector.get(TokenService)
+    token_service.clear_tokens()
