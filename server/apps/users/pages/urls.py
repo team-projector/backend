cca6d1b60f73eb016d3a753e21a11d2843dec863
Stanislav Lapshin <sm.lapshin@gmail.com>
2020-12-24 19:19:32 +0300

    Remove "Remove auth complete pages"
    This reverts commit 28d1756faa142ea8bb842cc66695bf3474cc4086.

---
+++ server/apps/users/pages/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from social_django import views
+
+from apps.users.pages.views import auth_complete
+
+app_name = "social"
+
+urlpatterns = [
+    path("login/<slug:backend>/", views.auth, name="begin"),
+    path("complete/<slug:backend>/", auth_complete, name="complete"),
+]
