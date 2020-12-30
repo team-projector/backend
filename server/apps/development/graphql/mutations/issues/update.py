58b69a08bc56aa88097f6924895d1c0d854b2136
Viacheslav Riazanov <riazanovslv@gmail.com>
2020-12-30 15:18:16 +0300

    Refactor UpdateIssueMutation #654

--- server/apps/development/graphql/mutations/issues/update.py
+++ server/apps/development/graphql/mutations/issues/update.py
@@ -1,47 +1,51 @@
-from typing import Any, Dict, Optional
+from typing import Dict, Optional
 
 import graphene
 from graphql import ResolveInfo
-from jnt_django_graphene_toolbox.mutations import BaseSerializerMutation
 from jnt_django_graphene_toolbox.security.permissions import AllowAuthenticated
-from rest_framework import serializers
 
-from apps.core.graphql.helpers.persisters import update_from_validated_data
-from apps.development.graphql.mutations.issues.inputs import BaseIssueInput
+from apps.core.graphql.mutations import BaseUseCaseMutation
 from apps.development.graphql.types import IssueType
-from apps.development.models import Ticket
-from apps.development.tasks import propagate_ticket_to_related_issues_task
+from apps.development.use_cases.issues import update as issue_update
 
 
-class InputSerializer(BaseIssueInput):
-    """InputSerializer."""
-
-    class Meta(BaseIssueInput.Meta):
-        fields = ("id", "ticket")
-
-    ticket = serializers.PrimaryKeyRelatedField(queryset=Ticket.objects.all())
-
-
-class UpdateIssueMutation(BaseSerializerMutation):
+class UpdateIssueMutation(BaseUseCaseMutation):
     """Update issue mutation."""
 
     class Meta:
-        serializer_class = InputSerializer
+        use_case_class = issue_update.UseCase
         permission_classes = (AllowAuthenticated,)
+
+    class Arguments:
+        id = graphene.ID(required=True)  # noqa: WPS125
+        ticket = graphene.ID(required=True)
 
     issue = graphene.Field(IssueType)
 
     @classmethod
-    def mutate_and_get_payload(  # type: ignore
+    def get_input_dto(
         cls,
         root: Optional[object],
-        info: ResolveInfo,  # noqa: WPS110ø
-        validated_data: Dict[str, Any],
-    ) -> "UpdateIssueMutation":
-        """Updating issue ticket."""
-        issue = validated_data.pop("issue")
+        info: ResolveInfo,  # noqa: WPS110
+        **kwargs,
+    ):
+        """Prepare use case input data."""
+        return issue_update.InputDto(
+            user=info.context.user,  # type: ignore
+            data=issue_update.IssueUpdateData(
+                issue=kwargs["id"],
+                ticket=kwargs["ticket"],
+            ),
+        )
 
-        update_from_validated_data(issue, validated_data)
-        propagate_ticket_to_related_issues_task.delay(issue_id=issue.pk)
-
-        return cls(issue=issue)
+    @classmethod
+    def get_response_data(
+        cls,
+        root: Optional[object],
+        info: ResolveInfo,  # noqa: WPS110
+        output_dto: issue_update.OutputDto,
+    ) -> Dict[str, object]:
+        """Prepare response data."""
+        return {
+            "issue": output_dto.issue,
+        }
